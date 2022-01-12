709d4be2cacaf0560877f84e3f24f87546e1e177
HEROgold <martijnwieringa28@gmail.com>
2022-01-12 23:05:54 +0100

    Added config

---
+++ config.py
@@ -0,0 +1,32 @@
+class main:
+    prefix = "$"
+    show_logged_in = True
+    log_messages = True
+    token = "TOKEN" #Example
+    owner_id = 12345679 # Example
+
+#class activity:
+#
+#class ban:
+#
+#class stats:
+#
+#class purge:
+#
+#class poll:
+#
+#class announcement:
+#
+#class welcome:
+#
+#class urban:
+#
+#class team:
+#
+class reminder:
+    min_duration = 1 #This is in minutes
+    max_duration = 365 #This is in days
+#
+#class invite:
+#
+#class welcome:
