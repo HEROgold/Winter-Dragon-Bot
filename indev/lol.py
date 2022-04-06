0a25c6f93f61a63018b740ac75ac27e584eb904c
HEROgold <martijnwieringa28@gmail.com>
2022-04-06 19:34:46 +0200

    Hero (#10)
    * Uppercase Change
    
    * Update stats.py
    
    * Config removal
    
    * Namechange of file + rpg increase
    
    * Channel message to dm
    
    * Created Would you rather.
    
    * Typo
    
    * Update wyr.py
    
    Added config, and ability to add questions
    
    * Removed config
    
    * Create translate.py
    
    * Update config.py
    
    * Allows config.py
    
    * removed printstatement
    
    * Update rpg.py
    
    * moved ban cog to in dev folder. lol .py too
    
    * Moved Rpg cog
    
    * Update lol.py
    
    * Update config.py
    
    * Update lol.py

--- indev/lol.py
+++ indev/lol.py
@@ -1,11 +1,12 @@
 import discord
 from discord.ext import commands
 import cassiopeia as cass
+import config
 
 class lol(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        cass.set_riot_api_key("RGAPI-a7107938-6c55-4758-857a-27fa2ea877b7")
+        cass.set_riot_api_key(config.lol.key)
 
     #@commands.Cog.listener()
     #EVENt LISTENER EXAPLE!
