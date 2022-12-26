c45064d05c445cc001e97bcc9cf6f5e43170a587
HEROgold <martijnwieringa28@gmail.com>
2022-12-26 21:24:19 +0100

    Annotations + config and cleanup

--- cogs/indev/message.py
+++ cogs/indev/message.py
@@ -4,6 +4,7 @@ from discord import app_commands
 from discord.ext import commands
 import json
 import os
+import config
 
 class get_message(commands.Cog):
     def __init__(self, bot:commands.Bot):
@@ -20,7 +21,7 @@ class get_message(commands.Cog):
             logging.info("Message Json Loaded.")
 
     # Helper functions to laod and update database
-    async def get_data(self) -> dict:
+    async def get_data(self) -> dict[str, dict[str, dict[str, str]]]:
         with open(self.DBLocation, 'r') as f:
             data = json.load(f)
         return data
@@ -29,7 +30,7 @@ class get_message(commands.Cog):
         with open(self.DBLocation,'w') as f:
             json.dump(data, f)
 
-    @app_commands.command(name = "get_messages", description = "get and store the last 400 messages, in each channel from this server!")
+    @app_commands.command(name = "get_messages", description = f"get and store the last {config.message.limit} messages, in each channel from this server!")
     async def slash_get_message(self, interaction:discord.Interaction):
         await interaction.response.defer(ephemeral=True)
         data = await self.get_data()
@@ -41,10 +42,16 @@ class get_message(commands.Cog):
             channel_id = str(channel.id)
             if channel_id not in data[guild_id]:
                 data[guild_id][channel_id] = {}
-            async for message in channel.history(limit=400):
+            async for message in channel.history(limit=config.message.limit):
                 if str(message.id) in data[guild_id][channel_id]:
                     break
+                if message.content in ["", "[Original Message Deleted]"]:
+                    # Skipt empty contents (I.e. Bot embeds etc.)
+                    continue
                 data[guild_id][channel_id][message.id] = str(message.content)
+            if not data[guild_id][channel_id]:
+                # Clean-up data if channel has no messages.
+                del data[guild_id][channel_id]
         await self.set_data(data)
         await interaction.followup.send("Updated my database!", ephemeral=True)
 
