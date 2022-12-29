30d310a97881a1fd3a0eb2ac6ddfe93ce6dfca13
HEROgold <martijnwieringa28@gmail.com>
2022-12-29 23:11:43 +0100

    Reminder Rewrite

--- cogs/user/reminder.py
+++ cogs/user/reminder.py
@@ -1,53 +1,88 @@
 import discord
-from discord.ext import commands
-import asyncio
+import logging
+import os
+import json
 import datetime
-import random
-
-# Switch to unix datetime and json format.
-# Rewrite
+from discord.ext import commands
+from discord import app_commands
+import asyncio
 
 class Reminder(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot) -> None:
+        super().__init__()
         self.bot:commands.Bot = bot
-    #lots of time converters inside command, then send message and wait the needed time to remind the user.
-    @commands.command(aliases=("remind","remember"),
-                    brief = "Remind yourself of something!",
-                    pass_context = True,
-                    description = "Set a reminder for yourself!, Time must end with s, h, d, m for seconds, hours, days, months.",
-                    usage = "`reminder [time] [message]`\n Example: `reminder 1d Father's birthday`")
-    async def reminder(self, ctx:commands.Context, time:str=None, *, reminder=None):
-        member = ctx.message.author
-        embed = discord.Embed(colour=(random.randint(0,16777215)))
-        seconds = 0
-        dm = await member.create_dm()
-        if reminder is None:
-            embed.add_field(name='Warning', value='Please specify what do you want me to remind you about.') # Error message
-        if time.lower().endswith("d"):
-            seconds += int(time[:-1]) * 60 * 60 * 24
-            counter = f"{seconds // 60 // 60 // 24} days"
-        if time.lower().endswith("h"):
-            seconds += int(time[:-1]) * 60 * 60
-            counter = f"{seconds // 60 // 60} hours"
-        elif time.lower().endswith("m"):
-            seconds += int(time[:-1]) * 60
-            counter = f"{seconds // 60} minutes"
-        elif time.lower().endswith("s"):
-            seconds += int(time[:-1])
-            counter = f"{seconds} seconds"
-        if seconds == 0:
-            embed.add_field(name='Warning', value='Please specify a proper duration, example: remind 5d give cactus water')
-        elif seconds < 60:
-            embed.add_field(name='Warning',
-                            value='You have specified a too short duration!\nMinimum duration is {config.min_duration} minute(s).')
-        elif seconds > 31556926:
-            embed.add_field(name='Warning', value='You have specified a too long duration!\nMaximum duration is {config.max_duration} days(s).')
+        self.DBLocation = "./Database/Reminder.json"
+        self.setup_db()
+
+    def setup_db(self):
+        if not os.path.exists(self.DBLocation):
+            with open(self.DBLocation, "w") as f:
+                data = {}
+                json.dump(data, f)
+                f.close
+                logging.info("Reminder Json Created.")
         else:
-            await dm.send(f"I will remind you about {reminder} in {counter}.")
-            await asyncio.sleep(seconds)
-            await dm.send(f"Hi, you asked me to remind you about {reminder}, {counter} ago.")
+            logging.info("Reminder Json Loaded.")
+
+    async def get_data(self) -> dict[str, list[dict[str, str|int]]]:
+        with open(self.DBLocation, 'r') as f:
+            data = json.load(f)
+        return data
+
+    async def set_data(self, data) -> None:
+        with open(self.DBLocation,'w') as f:
+            json.dump(data, f)
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.send_reminder()
+        await asyncio.sleep(60)
+        await self.on_ready()
+
+    async def send_reminder(self):
+        data = await self.get_data()
+        for member_id, remind_list in list(data.items()):
+            for i, remind_data in enumerate(remind_list):
+                if remind_data["unix_time"] <= datetime.datetime.now(datetime.timezone.utc).timestamp():
+                    member:discord.Member = discord.utils.get(self.bot.users, id=int(member_id))
+                    dm = await member.create_dm()
+                    reminder = remind_data["reminder"]
+                    await dm.send(f"I'm here to remind you about\n{reminder}")
+                    del remind_list[i]
+                    logging.info(f"Reminded {member}, and removed it.")
+            if not remind_list:
+                del data[member_id]
+                logging.info(f"Removing empty reminder for id {member_id}")
+        await self.set_data(data)
+
+    @app_commands.command(
+        name="remind",
+        description = "Set a reminder for yourself!",
+        )
+    async def slash_reminder(self, interaction:discord.Interaction, reminder:str, minutes:int=0, hours:int=0, days:int=0):
+        if minutes == 0 and hours == 0 and days == 0:
+            await interaction.response.send_message("Give me a time so i can remind you!", ephemeral=True)
             return
-        await dm.send(embed=embed)
+        else:
+            seconds = self.get_seconds(seconds=0, minutes=minutes, hours=hours, days=days)
+        member = interaction.user
+        data = await self.get_data()
+        time = (datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(seconds=seconds)).timestamp()
+        epoch = int(time)
+        member_id = str(member.id)
+        try:
+            reminders = data[member_id]
+            reminders.append({"reminder": reminder, "unix_time": epoch})
+        except KeyError:
+            reminders = data[member_id] = [{"reminder": reminder, "unix_time": epoch}]
+        await self.set_data(data)
+        await interaction.response.send_message(f"at <t:{epoch}> I will remind you of \n`{reminder}`", ephemeral=True)
+
+    def get_seconds(self, seconds, minutes, hours, days) -> int:
+        hours += days * 24
+        minutes += hours * 60
+        seconds += minutes * 60
+        return seconds
 
 async def setup(bot:commands.Bot):
 	await bot.add_cog(Reminder(bot))
