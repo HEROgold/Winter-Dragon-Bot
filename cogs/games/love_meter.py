48f4fb207b0590063bab133075a17a2c410c2272
HEROgold <martijnwieringa28@gmail.com>
2022-12-29 23:11:33 +0100

    Slash command
    Cormatted to slash commands

--- cogs/games/love_meter.py
+++ cogs/games/love_meter.py
@@ -1,35 +1,32 @@
 import discord
 import random
 from discord.ext import commands
+from discord import app_commands
 
 
 class love(commands.Cog):
     def __init__(self, bot):
         self.bot:commands.Bot = bot
 
-    @commands.cooldown(1, 1, commands.BucketType.user)
-    @commands.command(
+    @app_commands.command(
         name="love",
-        brief="Find out your compatability with another person!",
-        description="Use this command to find out if another person is compatible with you and your love",
-        usage="`love [user]`:\n Example: `love @user1`",
+        description="Find out if another person is compatible with you"
     )
-    async def love(self, ctx: commands.Context, *, content):
-        ind = 1 if ctx.message.mentions[0] == "<@742777596734996582>" else 0
+    async def love(self, interaction:discord.Interaction, member:discord.Member):
+        user = interaction.user
+        # Check if bot is mentioned and skip it
         emb = discord.Embed(
             title="Love Meter",
-            description="calculating your compatibility, this process took me some time, and i finally have an answer",
+            description=" ",
             colour=0xFF0000,
         )
-        for mention in ctx.message.mentions:
-            random.seed((ind + ctx.message.author.id))
-            emb.add_field(
-                name=f"{ctx.message.mentions[ind].display_name}",
-                value=f"Your compatibility with {ctx.message.mentions[ind].display_name} is {random.randint(0,100)}%",
-                inline=True,
-            )
-            ind += 1
-        await ctx.send(embed=emb)
+        random.seed((user.id + member.id))
+        emb.add_field(
+            name=f"{member.display_name}",
+            value=f"Your compatibility with {member.display_name} is {random.randint(0,100)}%",
+            inline=True,
+        )
+        await interaction.response.send_message(embed=emb)
 
 
 async def setup(bot: commands.Bot):
