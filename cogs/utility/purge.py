d41a41f5eb6493c82deb99639313107799d0dfd2
HEROgold <martijnwieringa28@gmail.com>
2022-12-29 11:10:39 +0100

    Update purge.py
    History delete instead of purge

--- cogs/utility/purge.py
+++ cogs/utility/purge.py
@@ -13,13 +13,19 @@ class Purge(commands.Cog):
         if count == -1:
             count = config.purge.limit
         if count <= config.purge.limit:
-            await interaction.channel.purge(limit=count)
-            await interaction.followup.send(f"Killed {count} Messages.", ephemeral=True)
+            # await interaction.channel.purge(limit=count)
+            messages = await self.history_delete(interaction=interaction, count=count)
+            await interaction.followup.send(f"Killed {len(messages)} Messages.", ephemeral=True)
         else:
             await interaction.followup.send(f"Too many message to kill! The limit is set to {config.purge.limit}", ephemeral=True)
 
-        # TODO: Use message history
-        # async for message in interaction.channel.history(limit=count):
+    async def history_delete(self, interaction:discord.Interaction, count:int) -> list[discord.Message]:
+        messages = []
+        async for amount, message in enumerate(interaction.channel.history(limit=count)):
+            message:discord.Message
+            await message.delete()
+            messages.append(message)
+        return messages
 
 async def setup(bot:commands.Bot):
 	await bot.add_cog(Purge(bot))
