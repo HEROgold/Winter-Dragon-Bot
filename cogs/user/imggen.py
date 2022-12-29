88ed2e03b506d4329e3523ea3f42058bce16118e
HEROgold <martijnwieringa28@gmail.com>
2022-12-29 23:12:02 +0100

    Cleanup random colors

--- cogs/user/imggen.py
+++ cogs/user/imggen.py
@@ -2,6 +2,8 @@ import logging
 import discord
 import os
 import asyncio
+import random
+import rainbow
 from craiyon import Craiyon
 from discord.ext import commands
 from discord import app_commands
@@ -11,7 +13,7 @@ class ImageGen(commands.Cog):
         self.CrAIyonDataBase = "./Database/img/crAIyon/"
         self.bot:commands.Bot = bot
 
-    async def generate_images(self, interaction:discord.Interaction, dm, query):
+    async def generate_images(self, interaction:discord.Interaction, dm:discord.DMChannel, query:str):
         generator = Craiyon() # Instantiates the api wrapper
         result = generator.generate(query) # Generates 9 images by default and you cannot change that
         if not os.path.exists(f"{self.CrAIyonDataBase}"):
@@ -19,7 +21,7 @@ class ImageGen(commands.Cog):
         os.mkdir(f"{self.CrAIyonDataBase}/{interaction.user.id}")
         result.save_images(f"{self.CrAIyonDataBase}{interaction.user.id}")
         for i in range(9):
-            embed=discord.Embed(title=f"AI Generated Image #{i}", color=0xffffff)
+            embed=discord.Embed(title=f"AI Generated Image #{i}", color=random.choice(rainbow.RAINBOW))
             embed.set_footer(text="These results come from an API, the results are from www.craiyon.com")
             await dm.send(file=discord.File(f"{self.CrAIyonDataBase}{interaction.user.id}/image-{i+1}.png"), embed=embed)
             os.remove(f"{self.CrAIyonDataBase}{interaction.user.id}/image-{i+1}.png")
@@ -33,9 +35,10 @@ class ImageGen(commands.Cog):
         dm = await interaction.user.create_dm()
         await interaction.response.defer(ephemeral=True)
         await interaction.followup.send("Proccessing images, please be patient.", ephemeral=True)
-        logging.info("Requesting images...")
+        logging.info(f"Requesting images for {interaction.user} with query {query}")
         # Run in another thread, to not get blocked on waiting generation
-        asyncio.run_coroutine_threadsafe(self.generate_images(interaction, dm, query), asyncio.new_event_loop())
+        # FIXME: RuntimeWarning: coroutine 'ImageGen.generate_images' was never awaited
+        # asyncio.run_coroutine_threadsafe(await self.generate_images(interaction, dm, query), asyncio.new_event_loop())
 
 async def setup(bot:commands.Bot):
     await bot.add_cog(ImageGen(bot))
