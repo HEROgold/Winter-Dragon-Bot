705e283f955ff3257ead68ff81d10016802412b8
HEROgold <martijnwieringa28@gmail.com>
2023-01-17 15:29:58 +0100

    Update main.py
    changed naming for config, and added guild paramater to the commands to push those to only the develop guild

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-from config import main as config
+import config
 import dragon_database
-# We make use of a config file, change values in config.py.
+
+
+# We make use of a config.Main file, change values in config.Main.py.
+# TODO: Push owner only commands in specific guild.
 
 LOG_LEVEL = logging.DEBUG
 
@@ -37,10 +40,12 @@ Intents.auto_moderation_configuration = True
 Intents.auto_moderation_execution = True
 
 client = discord.Client(intents=Intents)
-bot = commands.Bot(intents=Intents, command_prefix=commands.when_mentioned_or(config.prefix), case_insensitive=True)
+bot = commands.Bot(intents=Intents, command_prefix=commands.when_mentioned_or(config.Main.PREFIX), case_insensitive=True)
 tree = bot.tree
 
-if config.enable_custom_help:
+support_guild = bot.get_guild(config.Main.SUPPORT_GUILD_ID)
+
+if config.Main.CUSTOM_HELP:
     bot.remove_command("help")
 
 @bot.event
@@ -49,7 +54,7 @@ async def on_ready():
     # bot_username = await bot.user.edit(username="Winter Dragon")
     # bot_logger.info(f"Username updated to {bot_username}")
     print("Bot is running!")
-    if config.show_logged_in == True:
+    if config.Main.SHOW_LOGGED_IN == True:
         bot_logger.info(f'Logged on as {bot.user}!')
 
 async def innit():
@@ -92,7 +97,8 @@ async def mass_reload_cogs(interaction:discord.Interaction):
 
 @tree.command(
     name = "show_cogs",
-    description= "Show loaded cogs(For bot developer only)"
+    description= "Show loaded cogs (For bot developer only)",
+    guild = support_guild
     )
 async def slash_show_cogs(interaction:discord.Interaction):
     if not await bot.is_owner(interaction.user):
@@ -102,8 +108,9 @@ async def slash_show_cogs(interaction:discord.Interaction):
     await interaction.response.send_message(f"{cogs}", ephemeral=True)
 
 @tree.command(
-    name="reload",
-    description = "Reload a specified or all available cogs"
+    name = "reload",
+    description = "Reload a specified or all available cogs (For bot developer only)",
+    guild = support_guild
     )
 async def slash_restart(interaction:discord.Interaction, extension:str):
     if not await bot.is_owner(interaction.user):
@@ -129,7 +136,8 @@ async def restart_autocomplete_extension(interaction:discord.Interaction, curren
 
 @tree.command(
     name = "unload",
-    description = "Unload a specified cog"
+    description = "Unload a specified cog (For bot developer only)",
+    guild = support_guild
     )
 async def slash_unload(interaction:discord.Interaction, extension:str):
     if not bot.is_owner(interaction.user):
@@ -156,7 +164,8 @@ async def restart_autocomplete_extension(interaction:discord.Interaction, curren
 
 @tree.command(
     name = "load",
-    description = "Load a specified or all available cogs"
+    description = "Load a specified or all available cogs (For bot developer only)",
+    guild = support_guild
     )
 async def slash_load(interaction:discord.Interaction, extension:str):
     if not await bot.is_owner(interaction.user):
@@ -182,7 +191,7 @@ async def restart_autocomplete_extension(interaction:discord.Interaction, curren
 async def main():
     async with bot:
         await innit()
-        await bot.start(config.token)
+        await bot.start(config.Main.TOKEN)
 
 #run the bot!
 if __name__ == "__main__":
