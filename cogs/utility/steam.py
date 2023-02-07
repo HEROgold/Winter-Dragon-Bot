37ac1e11850dc70e1840e8a38c7292fc861d01f8
HEROgold <martijnwieringa28@gmail.com>
2023-02-07 10:09:19 +0100

    Update steam.py
    Logging updated, added some docstring, and regex for runegameid link

--- cogs/utility/steam.py
+++ cogs/utility/steam.py
@@ -3,6 +3,7 @@ import json
 import logging
 import os
 import random
+import re
 
 import discord
 import requests
@@ -32,18 +33,18 @@ class Steam(commands.GroupCog):
                 data = {"user_id" : []}
                 json.dump(data, f)
                 f.close
-                self.logger.debug(f"{self.database_name} Json Created.")
+                self.logger.info(f"{self.database_name} Json Created.")
         else:
-            self.logger.debug(f"{self.database_name} Json Loaded.")
+            self.logger.info(f"{self.database_name} Json Loaded.")
 
     def setup_html(self):
         if not os.path.exists(self.htmlFile):
             with open(self.htmlFile, "w") as f:
                 f.write("")
                 f.close()
-            self.logger.debug("Empty Steam Html created")
+            self.logger.info("Empty Steam Html created")
         else:
-            self.logger.debug("Steam Html found")
+            self.logger.info("Steam local Html exists")
 
     async def get_data(self) -> dict[str, list]:
         if config.Main.USE_DATABASE:
@@ -69,21 +70,26 @@ class Steam(commands.GroupCog):
     async def get_html(self, url=config.Steam.URL) -> str:
         requests.get(url)
         r = requests.get(url)
-        self.logger.debug("returning steam page")
+        self.logger.debug("Returning Steam html from url")
         return r.text
 
-    async def sale_from_html(self, html) -> list:
+    async def sale_from_html(self, html:str) -> list:
         sales = []
         soup = BeautifulSoup(html, "html.parser")
         for i in soup.find_all(class_="col search_discount responsive_secondrow"):
+            i:BeautifulSoup
             item = i.parent.parent.parent
             url = item["href"]
             title = item.find("span", {"class": "title"})
-            sale = i.find("span")
+            sale_amount = i.find("span") or sale_amount["text":"-1000%!"]
             try:
-                sales.append([title.text, sale.text, url])
+                # FIXME: Attribute error
+                # self.logger.debug(f"Sale_from_html: \ntitle='{title.text}',\nurl='{url}',\nsale='{sale_amount.text}'")
+                # self.logger.debug(f"HTML: item='{item}'")
+                sales.append([title.text, sale_amount.text, url])
             except Exception as e:
-                self.logger.exception("Could not append:", e)
+                self.logger.exception(f"Could not append: {e}")
+                continue
         return sales
 
     async def dupe_check(self, html:str, html_file_path:str) -> bool:
@@ -91,44 +97,50 @@ class Steam(commands.GroupCog):
         from_html = await self.sale_from_html(html)
         a_hundred, b_hundred = self.find_100_sales(from_file, from_html)
         if from_file == []:
-            self.logger.debug("Copied SteamPage.html is empty")
+            self.logger.debug("Copied SteamPage.html is empty.")
             return False
         if from_html == from_file:
             self.logger.debug("Steam File and Html are the same.")
             return True
         elif a_hundred == b_hundred:
-            self.logger.debug("Sales from File and Html are the same")
+            self.logger.debug("Sales from File and Html are the same.")
             return True
         else:
-            self.logger.debug("Steam File and Html are not the same.")
-            return await self.check_new(a_hundred, b_hundred)
+            self.logger.debug("Steam File and Html not the same. Checking for new sales.")
+            return not await self.check_new(a_hundred, b_hundred)
 
     async def sale_from_file(self, html_file_path):
         with open(html_file_path, "r", encoding="utf-8") as f:
             from_file = await self.sale_from_html(f.read())
         return from_file
 
-    def find_100_sales(self, from_file, from_html) -> list[list]:
+    def find_100_sales(self, from_file:list, from_html:list) -> tuple[list, list]:
         a = []
         b = []
-        try:
-            for i,j in zip(from_file, from_html):
-                # Keep both lists same size!
-                if i[1] == "-100%" or j[1] == "-100%":
-                    a.append(i)
-                    b.append(j)
-        except Exception as e:
-            self.logger.exception(e)
+        for i,j in zip(from_file, from_html):
+            # Keep both lists same size!
+            if i[1] == "-100%" or j[1] == "-100%":
+                a.append(i)
+                b.append(j)
         a.sort()
         b.sort()
         self.logger.debug(f"SteamLists: {a}, {b}")
         return a, b
 
     async def check_new(self, from_file:list[list], from_html:list[list]) -> bool:
+        """Check if sale from html is not found inside the saved html file
+
+        Args:
+            from_file (List): Sales from saved file
+            from_html (List): Sales from requested url
+
+        Returns:
+            bool: True when new sale is found, false on loop ending
+        """
         self.logger.debug(f"Checking for new item(s): a={from_file} b={from_html}")
         for sale in from_html:
             if sale not in from_file and sale[1] == "-100%":
-                self.logger.debug(f"new from_html: {sale}")
+                self.logger.info(f"New sale found from_html: {sale}")
                 return True
         return False
 
@@ -150,7 +162,7 @@ class Steam(commands.GroupCog):
             f.close()
         sales_html = await self.sale_from_html(html)
         embed = discord.Embed(title="Free Steam Game's", description="New free Steam Games have been found!", color=random.choice(rainbow.RAINBOW))
-        embed.set_footer(text="You can disable this by using `/remove_free_steam`")
+        embed.set_footer(text="You can disable this by using `/steam remove `")
         embed = await self.populate_embed(sales_html, embed)
         data = await self.get_data()
         for id in data["user_id"]:
@@ -177,10 +189,19 @@ class Steam(commands.GroupCog):
             if i[1] == "-100%":
                 name = i[0]
                 field_val = i[2]
-                embed.add_field(name=name, value=field_val, inline=False)
+                regex_game_id = "(?:https?:\/\/)?store\.steampowered\.com\/app\/(\d+)\/[a-zA-Z0-9_\/]+"
+                game_id = re.findall(regex_game_id, field_val)
+                self.logger.debug(f"regex game_id=`{game_id}`")
+                # TODO: Check this on next sale!
+                # 'https://store.steampowered.com/app/2279791/Forspoken_Cats_Meow_Cloak/?snr=1_7_7_2300_150_1'
+                # https://store.steampowered.com/
+                # then add steam://rungameid/2279791
+                run_game_id = f"steam://rungameid/{game_id[0]}"
+                embed.add_field(name=name, value=f"{field_val}\nInstall here:{run_game_id}", inline=False)
         return embed
 
     @app_commands.command(name = "show", description= "Get a list of 100% Sale steam games.")
+    @app_commands.checks.cooldown(1, 300)
     async def slash_show(self, interaction: discord.Interaction):
         html = await self.get_html()
         sales_html = await self.sale_from_html(html)
