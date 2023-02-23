a8ecb1e727ac4a8db129ac38575a083271c58c8c
HEROgold <martijnwieringa28@gmail.com>
2023-02-24 00:04:39 +0100

    Removed broken library, using requests

--- cogs/utility/urban.py
+++ cogs/utility/urban.py
@@ -1,5 +1,8 @@
 import discord
-import urbandictionary as ud
+import requests
+import urllib.parse
+import logging
+
 from discord import app_commands
 from discord.ext import commands
 
@@ -8,6 +11,7 @@ import config
 class Urban(commands.GroupCog):
     def __init__(self, bot) -> None:
         self.bot:commands.Bot = bot
+        self.logger = logging.getLogger(f"{config.Main.BOT_NAME}.{self.__class__.__name__}")
 
     @app_commands.command(
         name="random",
@@ -15,43 +19,55 @@ class Urban(commands.GroupCog):
     )
     async def slash_urban_random(self, interaction:discord.Interaction) -> None:
         if config.Urban.ALLOW_RANDOM == True:
-            random = ud.random()
+            UD_RANDOM_URL = 'http://api.urbandictionary.com/v0/random'
+            response = requests.get(UD_RANDOM_URL)
+            json = response.json()
+            random_list:list[dict] = json["list"]
+            self.logger.debug(f"{random_list}")
             emb = discord.Embed(title="Dictionary Random")
-            emb.set_footer(text="Results are from an API, I am not responsable for results!")
+            emb.set_footer(text="Results are from an API!")
             emb.set_author(name=interaction.user.display_name, icon_url=interaction.user.avatar)
-            for i, r in enumerate(random, start=1):
+            for i, r in enumerate(random_list, start=1):
                 if i <= config.Urban.MAX_LENGHT:
-                    emb.add_field(name=r.word, value=r.definition, inline=True)
+                    word = r["word"]
+                    definition = r["definition"]
+                    urban_url = r["permalink"]
+                    self.logger.debug(f"word: {word}, definition: {definition}")
+                    emb.add_field(name=word, value=f"{definition}\n{urban_url}", inline=True)
             await interaction.response.send_message(embed=emb)
         else:
             await interaction.response.send_message("Random definitions are disabled", ephemeral=True)
 
-    # FIXME: discord.app_commands.errors.CommandInvokeError: Command 'search' raised an exception: KeyError: 'result_type'
     @app_commands.command(
         name="search",
         description="Look into urban dictionary for a meaning and its definition."
         )
     async def slash_urban(self, interaction:discord.Interaction, query:str) -> None:
-        defined = ud.define(query)
+        UD_DEFINE_URL = 'http://api.urbandictionary.com/v0/define?term='
+        response = requests.get(UD_DEFINE_URL + urllib.parse.quote(query))
+        json = response.json()
+        # defined_list:list[dict] = json["list"]
+        defined = json["list"]
+        self.logger.debug(f"defined: {defined}")
         if len(defined) > 1:
-            emb = discord.Embed(title=f"Dictionary Search {query}")
-            emb.set_footer(text="Results are from an API, I am not responsable for results!")
+            emb = discord.Embed(title=f"Dictionary Search: {query}")
+            emb.set_footer(text="Results are from an API!")
             emb.set_author(name=interaction.user.display_name, icon_url=interaction.user.avatar)
-            emb.add_field(name="1", value=defined[0].definition, inline=True)
-            emb.add_field(name="2", value=defined[1].definition, inline=True)
+            emb.add_field(name="1", value=defined[0]["definition"], inline=True)
+            emb.add_field(name="2", value=defined[1]["definition"], inline=True)
             emb.add_field(name="|", value="|", inline=False)
-            emb.add_field(name="Votes 1:", value=f":thumbsup: {str(defined[0].upvotes)} :thumbsdown:{str(defined[0].downvotes)}", inline=True)
-            emb.add_field(name="Votes 2:", value=f":thumbsup: {str(defined[1].upvotes)} :thumbsdown:{str(defined[1].downvotes)}", inline=True)
-            await interaction.followup.send(embed=emb)
+            emb.add_field(name="Votes 1:", value=f":thumbsup: {str(defined[0]['thumbs_up'])} :thumbsdown:{str(defined[0]['thumbs_down'])}", inline=True)
+            emb.add_field(name="Votes 2:", value=f":thumbsup: {str(defined[1]['thumbs_up'])} :thumbsdown:{str(defined[1]['thumbs_down'])}", inline=True)
+            await interaction.response.send_message(embed=emb)
         elif len(defined) == 1:
             emb = discord.Embed(title=f"Dictionary Search {query}")
-            emb.set_footer(text="Results are from an API, I am not responsable for results!")
-            emb.set_author(name=interaction.user.display_name, icon_url=interaction.user.avatar_url)
-            emb.add_field(name="1", value=defined[0].definition, inline=False)
-            emb.add_field(name="Votes 1:", value=f":thumbsup: {str(defined[0].upvotes)} :thumbsdown:{str(defined[0].downvotes)}", inline=False)
-            await interaction.followup.send(embed=emb)
+            emb.set_footer(text="Results are from an API!")
+            emb.set_author(name=interaction.user.display_name, icon_url=interaction.user.avatar.url)
+            emb.add_field(name="1", value=defined[0]["definition"], inline=False)
+            emb.add_field(name="Votes 1:", value=f":thumbsup: {str(defined[0]['thumbs_up'])} :thumbsdown:{str(defined[0]['thumbs_down'])}", inline=False)
+            await interaction.response.send_message(embed=emb)
         else:
-            await interaction.followup.send("No results found.")
+            await interaction.response.send_message("No results found.")
 
 async def setup(bot:commands.Bot) -> None:
 	await bot.add_cog(Urban(bot))
