65a53a5038f0b94ec9ff8d84fb0d4c54fd6e989f
HEROgold <martijnwieringa28@gmail.com>
2023-03-10 04:37:10 +0100

    Updated formatting using Black

--- template.py
+++ template.py
@@ -1,8 +1,8 @@
-import pickle
 import logging
 import os
+import pickle
 
-import discord # type: ignore
+import discord  # type: ignore
 from discord import app_commands
 from discord.ext import commands
 
@@ -11,9 +11,11 @@ import tools.dragon_database as dragon_database
 
 
 class Temp(commands.GroupCog):
-    def __init__(self, bot:commands.Bot) -> None:
+    def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-        self.logger = logging.getLogger(f"{config.Main.BOT_NAME}.{self.__class__.__name__}")
+        self.logger = logging.getLogger(
+            f"{config.Main.BOT_NAME}.{self.__class__.__name__}"
+        )
         self.data = None
         self.DATABASE_NAME = self.__class__.__name__
         if not config.Main.USE_DATABASE:
@@ -56,16 +58,14 @@ class Temp(commands.GroupCog):
     # TEMP_GROUP = app_commands.Group(name="TEMPGroup", description="TEMP")
     # @TEMP_GROUP.command()
 
-
-    @app_commands.command(
-        name="TEMP",
-        description="TEMP"
-    )
-    async def slash_TEMP(selfself, interaction:discord.Interaction) -> None:
+    @app_commands.command(name="TEMP", description="TEMP")
+    async def slash_TEMP(selfself, interaction: discord.Interaction) -> None:
         pass
 
     @slash_TEMP.autocomplete("")
-    async def COMMAND_autocoplete_VARIABLE(self, interaction:discord.Interaction, current:str) -> list[app_commands.Choice[str]]:
+    async def COMMAND_autocoplete_VARIABLE(
+        self, interaction: discord.Interaction, current: str
+    ) -> list[app_commands.Choice[str]]:
         return [
             app_commands.Choice(name=i, value=i)
             for i in []
@@ -73,5 +73,5 @@ class Temp(commands.GroupCog):
         ]
 
 
-async def setup(bot:commands.Bot) -> None:
-    await bot.add_cog(Temp(bot)) # type: ignore
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(Temp(bot))  # type: ignore
