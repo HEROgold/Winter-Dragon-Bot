ea96d1ec0842a01d26426771b201214d6f488509
HEROgold <martijnwieringa28@gmail.com>
2023-06-08 23:01:52 +0200

    Added control for SIGINT, removing old logs

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import asyncio
 import logging
 import os
+import re
 import shutil
+import signal
 import sys
 from atexit import register
 from datetime import datetime
@@ -11,22 +13,16 @@ from discord.ext import commands
 
 import config
 
-# Change values/settings in config.py.
-# TODO: Push owner only commands in specific guild.
-# TODO: write dpy/pytests
-
-LOG_LEVEL = logging.DEBUG
-
 bot_logger = logging.getLogger(f"{config.Main.BOT_NAME}")
 discord_logger = logging.getLogger('discord')
 
-bot_logger.setLevel(LOG_LEVEL)
+bot_logger.setLevel(config.Main.LOG_LEVEL)
 bot_handler = logging.FileHandler(filename='bot.log', encoding='utf-8', mode='w')
 bot_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 bot_logger.addHandler(bot_handler)
 bot_logger.addHandler(logging.StreamHandler())
 
-discord_logger.setLevel(logging.INFO)
+discord_logger.setLevel(config.Main.LOG_LEVEL)
 discord_handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 discord_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 discord_logger.addHandler(discord_handler)    
@@ -44,12 +40,14 @@ Intents.auto_moderation_configuration = True
 Intents.auto_moderation_execution = True
 
 client = discord.Client(intents=Intents)
-bot = commands.Bot(intents=Intents, command_prefix=commands.when_mentioned_or(config.Main.PREFIX), case_insensitive=True)
+# bot = commands.Bot(intents=Intents, command_prefix=commands.when_mentioned_or(config.Main.PREFIX), case_insensitive=True)
+bot = commands.AutoShardedBot(intents=Intents, command_prefix=commands.when_mentioned_or(config.Main.PREFIX), case_insensitive=True)
 tree = bot.tree
 
 
 if config.Main.CUSTOM_HELP:
     bot.remove_command("help")
+
 
 @bot.event
 async def on_ready() -> None:
@@ -57,11 +55,12 @@ async def on_ready() -> None:
     if config.Main.SHOW_LOGGED_IN == True:
         bot_logger.info(f'Logged on as {bot.user}!')
 
+
 async def main() -> None:
     async with bot:
         await mass_load()
-        await bot.load_extension("jishaku")
-        await bot.start(config.Tokens.DICSORD_TOKEN)
+        # await bot.load_extension("jishaku")
+        await bot.start(config.Tokens.DISCORD_TOKEN)
 
 
 def logs_size_limit_check(size_in_kb: int) -> bool:
@@ -72,12 +71,9 @@ def logs_size_limit_check(size_in_kb: int) -> bool:
     )
     bot_logger.debug(f"{total_size=} {size_in_kb=}")
     return total_size > size_in_kb
-    # if returns true, def function to remove the oldest logs
+
 
 def delete_oldest_logs() -> None:
-    # TODO: 
-    # refactor code, store sizes in dict, then remove oldest X needed
-    # to stay under set limit.
     oldest_files = sorted(
         (
             os.path.join(root, file)
@@ -86,14 +82,16 @@ def delete_oldest_logs() -> None:
         ),
         key=os.path.getctime,
     )
-    bot_logger.info(f"deleting old log for space: {oldest_files}")
-    os.remove(oldest_files[0])
-    # TODO: remove empty directories
-    # https://stackoverflow.com/questions/10989005/do-i-understand-os-walk-right
-    # for root, dirs, files in os.walk(config.Main.LOG_PATH):
-    #     for dir in dirs:
-    #         for file in files:
-    #             os.path.join(root, dirs, file)
+    # Some regex magic https://regex101.com/r/he2KNZ/1
+    # './logs\\2023-05-08-00-10-27\\bot.log' matches into
+    # /logs\\2023-05-08-00-10-27\\
+    regex = r"(\./logs)(/|\d|-)+"
+    folder_path = re.match(regex, oldest_files[0])[0]
+    bot_logger.info(f"deleting old logs for space: {folder_path=}")
+    for file in os.listdir(folder_path):
+        os.remove(f"{folder_path}{file}")
+    os.rmdir(folder_path)
+
 
 def save_logs() -> None:
     while logs_size_limit_check(config.Main.LOG_SIZE_KB_LIMIT):
@@ -103,21 +101,29 @@ def save_logs() -> None:
     log_time = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
     if not os.path.exists(f"{config.Main.LOG_PATH}/{log_time}"):
         os.mkdir(f"{config.Main.LOG_PATH}/{log_time}")
-    bot_logger.info("Saved log files")
-    shutil.copy(src="bot.log", dst=f"{config.Main.LOG_PATH}/{log_time}/bot.log")
-    shutil.copy(src="discord.log", dst=f"{config.Main.LOG_PATH}/{log_time}/discord.log")
+    bot_logger.info("Saving log files")
+    logging.shutdown()
+    for file in os.listdir("./"):
+        if file.endswith(".log"):
+            print(file)
+            shutil.copy(src=f"./{file}", dst=f"{config.Main.LOG_PATH}/{log_time}/{file}")
+
 
 async def get_cogs() -> list[str]:
     extensions = []
     for root, _, files in os.walk("cogs"):
         extensions.extend(
-            os.path.join(root, file[:-3]).replace("\\", ".")
+            os.path.join(root, file[:-3]).replace("/", ".")
             for file in files
             if file.endswith(".py")
         )
     return extensions
 
+
 async def mass_load() -> None:
+    if not (os.listdir("./cogs")):
+        bot_logger.critical("No Cogs Directory To Load!")
+        return
     cogs = await get_cogs()
     for cog in cogs:
         try:
@@ -125,26 +131,26 @@ async def mass_load() -> None:
             bot_logger.info(f"Loaded {cog}")
         except Exception as e:
             bot_logger.exception(e)
-    if not (os.listdir("./cogs")):
-        bot_logger.warning("No Cogs Directory To Load!")
 
-@tree.command(
-    name = "shutdown",
-    description = "(For bot developer only)"
-    )
-async def slash_shutdown(interaction:discord.Interaction) -> None:
+
+@tree.command(name = "shutdown", description = "(For bot developer only), since it runs it docker. It restarts!")
+async def slash_shutdown(interaction: discord.Interaction) -> None:
     if not await bot.is_owner(interaction.user):
         raise commands.NotOwner
-    await interaction.response.send_message("Shutting down.", ephemeral=True)
-    bot_logger.debug("shutdown by command.")
+    try:
+        await interaction.response.send_message("Shutting down.", ephemeral=True)
+    except Exception:
+        pass
+    bot_logger.info("shutdown by command.")
     save_logs()
     await bot.close()
     await client.close()
     sys.exit()
 
+
 @register
 def terminate() -> None: 
-    bot_logger.info("Logging off")
+    bot_logger.info("Logging off due to terminate")
     save_logs()
     try:
         asyncio.run(bot.close())
@@ -155,6 +161,9 @@ def terminate() -> None:
         pass
     sys.exit()
 
+
 #run the bot!
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, terminate)
+    signal.signal(signal.SIGTERM, terminate)
     asyncio.run(main())
