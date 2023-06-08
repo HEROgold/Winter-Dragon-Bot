856bb11e376e455c84132e76da30515287b10c77
HEROgold <martijnwieringa28@gmail.com>
2023-06-08 23:34:05 +0200

    Added hybrid command

--- cogs/extension/sync.py
+++ cogs/extension/sync.py
@@ -6,9 +6,6 @@ from discord.ext import commands, tasks
 
 import config
 
-    # TODO: Make Hybrid command
-    # @commands.hybrid_command(name="sync", description="Sync all commands on all servers (Bot dev only)")
-
 
 class Sync(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
@@ -49,6 +46,24 @@ class Sync(commands.Cog):
         global_list.sort()
         local_list.sort()
         await interaction.response.send_message(f"Sync complete\nGlobally synced: {global_list}\nLocally synced: {local_list} to {guild}", ephemeral=True)
+
+
+    # TODO: Make Hybrid command
+    @commands.hybrid_command(name="sync_ctx", description="Sync all commands on all servers (Bot dev only)")
+    async def slash_sync(self, ctx: commands.Context) -> None:
+        if not await self.bot.is_owner(ctx.author):
+            raise commands.NotOwner
+        global_sync = await self.bot.tree.sync()
+        guild = self.bot.get_guild(config.Main.SUPPORT_GUILD_ID)
+        local_sync = await self.bot.tree.sync(guild=guild)
+        self.logger.warning(f"{ctx.author} Synced slash commands!")
+        self.logger.debug(f"Synced commands: global_commands={global_sync} guild_commands={local_sync}")
+        global_list = [command.name for command in global_sync]
+        local_list = [command.name for command in local_sync]
+        global_list.sort()
+        local_list.sort()
+        await ctx.send(f"Sync complete\nGlobally synced: {global_list}\nLocally synced: {local_list} to {guild}")
+
 
 
 async def setup(bot: commands.Bot) -> None:
