b30071521e66698b0de64909581f612672d4dc8c
HEROgold <martijnwieringa28@gmail.com>
2023-06-24 00:17:53 +0200

    added spacing

--- cogs/user/invite.py
+++ cogs/user/invite.py
@@ -13,6 +13,7 @@ class Invite(commands.GroupCog):
         self.bot = bot
         self.logger = logging.getLogger(f"{config.Main.BOT_NAME}.{self.__class__.__name__}")
 
+
     @app_commands.command(
         name="bot",
         description="Invite this bot to your own server!",
@@ -21,15 +22,17 @@ class Invite(commands.GroupCog):
         self.logger.debug(f"Invite created for: id=`{interaction.user.id}`")
         await interaction.response.send_message("https://discord.com/api/oauth2/authorize?client_id=742777596734996582&permissions=4398046511095&scope=bot", ephemeral=True)
 
+
     @app_commands.command(
         name="server",
         description="get invited to the official support server"
     )
     async def slash_support(self, interaction: discord.Interaction) -> None:
-        guild:discord.Guild = self.bot.get_guild(config.Main.SUPPORT_GUILD_ID)
-        channel = guild.system_channel is not None or guild.channels[0]
+        guild: discord.Guild = self.bot.get_guild(config.Main.SUPPORT_GUILD_ID) or await self.bot.fetch_guild(config.Main.SUPPORT_GUILD_ID)
+        channel = guild.system_channel or guild.channels[0]
         invite = await channel.create_invite(max_uses=1, max_age=60, reason=f"Support command used by {interaction.user.mention}")
         await interaction.response.send_message(invite.url, ephemeral=True)
+
 
 async def setup(bot: commands.Bot) -> None:
     # sourcery skip: instance-method-first-arg-name
