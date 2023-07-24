6bedfab6aacb7867c1b341191760ed75b209190e
HEROgold <martijnwieringa28@gmail.com>
2023-07-24 16:40:31 +0200

    fixed config values

--- extensions/server/purge.py
+++ extensions/server/purge.py
@@ -18,8 +18,8 @@ class Purge(commands.Cog):
     @app_commands.checks.has_permissions(manage_messages=True, manage_channels=True)
     async def slash_purge(self, interaction: discord.Interaction, count: int, use_history: bool = False) -> None:
         if count == -1:
-            count = config["Purge"]["LIMIT"]
-        if count <= config["Purge"]["LIMIT"]:
+            count = int(config["Purge"]["limit"])
+        if count <= int(config["Purge"]["limit"]):
             await interaction.response.defer()
             history_messages_count = 0
             purged_count = 0
@@ -28,7 +28,7 @@ class Purge(commands.Cog):
             self.logger.debug(f"Purged: {purged_count}")
             if (
                 purged_count < count
-                and config["Purge"]["USE_HISTORY"] == True
+                and config["Purge"]["use_history"] == "True"
                 and use_history
             ):
                 history_messages = await self.history_delete(interaction=interaction, count=(count - purged_count))
