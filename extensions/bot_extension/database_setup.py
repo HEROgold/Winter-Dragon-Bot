f11d089352ecbc90525cd42caada74be0778390d
HEROgold <martijnwieringa28@gmail.com>
2023-08-12 21:39:17 +0200

    switched to isinstance

--- extensions/bot_extension/database_setup.py
+++ extensions/bot_extension/database_setup.py
@@ -4,6 +4,7 @@ import logging
 import discord  # type: ignore
 from discord import app_commands
 from discord.ext import commands, tasks
+from tools import event_errors
 
 from tools.config_reader import config
 from tools.database_tables import Session, engine, Channel, Guild, Message, User, Presence
@@ -34,13 +35,13 @@ class DatabaseSetup(commands.Cog):
 
 
     @commands.Cog.listener()
+    @event_errors.event_logger
     async def on_message(self, message: discord.Message) -> None:
         user = message.author
         guild = message.guild
         channel = message.channel
 
-        # TODO: if this works, remove comment. if not. use isinstance()
-        if channel == discord.DMChannel:
+        if isinstance(channel, discord.DMChannel):
             self.logger.debug(f"{type(channel)=}")
             return
 
