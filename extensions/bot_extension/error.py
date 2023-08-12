f242f2fe44e54cedc889e133318c5da368c3f684
HEROgold <martijnwieringa28@gmail.com>
2023-08-12 18:37:20 +0200

    added todo

--- extensions/bot_extension/error.py
+++ extensions/bot_extension/error.py
@@ -135,6 +135,7 @@ class ErrorHandler:
         self.logger.debug(f"{type(error)=}, {error.args=}")
         
         if config["Error"]["always_log_errors"] == "True":
+            # TODO: add full traceback
             self.logger.exception(f"always log: {error}")
         if config["Error"]["ignore_errors"] == "True":
             return
