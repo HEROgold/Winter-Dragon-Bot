fbb11a3441f437afcbb3aa66c7b5cf2fd0a2152f
HEROgold <martijnwieringa28@gmail.com>
2023-08-17 04:04:47 +0200

    added async wrapper, and fixme

--- tools/event_errors.py
+++ tools/event_errors.py
@@ -3,12 +3,23 @@
 
 # TODO: Needs testing
 
+from asyncio import iscoroutinefunction
 import inspect
 import logging
 
 from .config_reader import config
 
-def event_logger(func: function):
+
+# FIXME: Extension 'extensions.bot_extension.database_setup' raised an error: AttributeError: 'DatabaseSetup' object has no attribute 'async_wrapper'
+def event_logger(func): # : function
+    async def async_wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            print(f"Error in {inspect.getfile(func)}, in {func.__class__} at {func.__name__}:\n{e}")
+            logger = func.__class__.__dict__.get("logger", logging.getLogger(f"{config['Main']['bot_name']}.event_errors"))
+            logger.exception(f"Error in {inspect.getfile(func)}, in {func.__class__} at {func.__name__}:\n{e}")
+
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
@@ -16,4 +27,5 @@ def event_logger(func: function):
             print(f"Error in {inspect.getfile(func)}, in {func.__class__} at {func.__name__}:\n{e}")
             logger = func.__class__.__dict__.get("logger", logging.getLogger(f"{config['Main']['bot_name']}.event_errors"))
             logger.exception(f"Error in {inspect.getfile(func)}, in {func.__class__} at {func.__name__}:\n{e}")
-    return wrapper
+
+    return async_wrapper if iscoroutinefunction(func) else wrapper
