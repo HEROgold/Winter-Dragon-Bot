5141cae7605e5f43f2bb85f7e53d44439f7b0a0a
HEROgold <martijnwieringa28@gmail.com>
2023-09-15 19:26:43 +0200

    removed auto sync (no need, didnt work)

--- extensions/bot_extension/sync.py
+++ extensions/bot_extension/sync.py
@@ -1,6 +1,6 @@
 import discord
 from discord import app_commands
-from discord.ext import commands, tasks
+from discord.ext import commands
 
 from tools.config_reader import config
 from _types.cogs import Cog
@@ -8,59 +8,6 @@ from _types.bot import WinterDragon
 
 
 class Sync(Cog):
-    async def cog_load(self) -> None:
-        self.update.start()
-
-
-    # Seems to work properly
-    @tasks.loop(count=1)
-    async def update(self) -> None:
-        # sourcery skip: useless-else-on-loop
-        app_commands = await self.bot.tree.fetch_commands()
-        commands = list(self.bot.tree.walk_commands())
-        synced = [
-            cmd.name 
-            for cmd in commands
-            for app_cmd in app_commands
-            if app_cmd.name == cmd.name
-            # or cmd.parent.name == app_cmd.name
-        ]
-        # synced shows wrong list
-        self.logger.debug(f"{synced=}")
-        self.logger.debug(f"{[i.name for i in app_commands]=}")
-
-        if not commands:
-            self.logger.critical("No commands found")
-            return
-        if len(app_commands) == 0 or not synced:
-            self.logger.warning("No synced commands found, automatically syncing")
-            # await self.bot.tree.sync()
-            return
-
-        # goal: for each app_command thats found, but not in commands, == de-synced
-        # goal: for each command not in app_commands == de-synced
-        for app_command in app_commands:
-            self.logger.debug(f"checking {app_command.name}")
-            for command in commands:
-                if command.parent is not None:
-                    continue
-                if command.name == app_command.name:
-                    self.logger.debug(f"cmd.name in app_cmd: {app_command.name=}, {command.name=}")
-                    break
-            else:
-                self.logger.debug(f"cmd.name not in app_cmd: {app_command.name=}, {command.name=}")
-                break
-            continue
-        else:
-            return
-        self.logger.warning("De-sync found, re-syncing")
-        # await self.bot.tree.sync()
-
-
-    @update.before_loop # type: ignore
-    async def before_update(self) -> None:
-        self.logger.info("Waiting until bot is online")
-        await self.bot.wait_until_ready()
 
 
     @app_commands.command(name="sync", description="Sync all commands on all servers (Bot dev only)")
