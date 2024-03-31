a38417506543d231c45f291a710abdbd4802270e
HEROgold <martijnwieringa28@gmail.com>
2024-03-31 03:22:32 +0200

    format

--- tools/utils.py
+++ tools/utils.py
@@ -4,15 +4,16 @@ from typing import Any, TypeVar
 
 T = TypeVar("T")
 
+
 def get_arg(args: Sequence[Any], target: T) -> T | None:
     return next(
-    (
-        arg
-        for arg in args
-        if isinstance(
-            arg,
-            target, # type: ignore
-        )
-    ),
-    None,
+        (
+            arg
+            for arg in args
+            if isinstance(
+                arg,
+                target,  # type: ignore
+            )
+        ),
+        None,
     )
