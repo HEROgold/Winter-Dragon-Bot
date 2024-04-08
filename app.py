625bfad5179524223c2e07c194a57079bae45a9c
HEROgold <martijnwieringa28@gmail.com>
2024-04-08 17:14:04 +0200

    added home

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from blueprints import ctrl, docs, page
 from tools.config_reader import STATIC_PATH, TEMPLATE_PATH
@@ -7,3 +7,7 @@ from tools.flask_tools import register_blueprints
 
 app = Flask(__name__, template_folder=TEMPLATE_PATH, static_folder=STATIC_PATH)
 register_blueprints(app, [ctrl, docs, page])
+
+@app.route("/")
+def index() -> str:
+    return render_template("index.j2")
