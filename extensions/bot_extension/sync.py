34a4567a8eb0fb1c7cdd4181d0ebbdfc9dda6c12
HEROgold <martijnwieringa28@gmail.com>
2024-04-08 17:00:57 +0200

    update sync

--- extensions/bot_extension/sync.py
+++ extensions/bot_extension/sync.py
@@ -1,44 +1,55 @@
+from typing import TYPE_CHECKING
+
 import discord
 from discord import app_commands
 from discord.ext import commands
 
 from _types.bot import WinterDragon
 from _types.cogs import Cog
-from tools.config_reader import config
+
+
+if TYPE_CHECKING:
+    from discord.app_commands.models import AppCommand
 
 
 class Sync(Cog):
+    @app_commands.command(name="sync", description="Sync all commands on this server")
+    async def slash_sync(self, interaction: discord.Interaction | commands.Context) -> None:
+        user = interaction.author if isinstance(interaction, commands.Context) else interaction.user
 
+        guild = interaction.guild
 
-    @app_commands.command(name="sync", description="Sync all commands on all servers (Bot dev only)")
-    @app_commands.guilds(config.getint("Main", "support_guild_id"))
-    @commands.is_owner()
-    async def slash_sync(self, interaction: discord.Interaction) -> None:
-        global_sync = await self.bot.tree.sync()
-        guild = self.bot.get_guild(config.getint("Main", "support_guild_id"))
         local_sync = await self.bot.tree.sync(guild=guild)
-        self.logger.warning(f"{interaction.user} Synced slash commands!")
-        self.logger.debug(f"Synced commands: {global_sync=}, {local_sync=}")
-        global_list = [command.name for command in global_sync]
+
+        self.logger.warning(f"{user} Synced slash commands!")
+        self.logger.debug(f"Synced commands: {local_sync}")
         local_list = [command.name for command in local_sync]
-        global_list.sort()
         local_list.sort()
-        await interaction.response.send_message(f"Sync complete\nGlobally synced: {global_list}\nLocally synced: {local_list} to {guild}", ephemeral=True)
+        await interaction.response.send_message(f"Sync complete\nSynced: {local_list} to {guild}", ephemeral=True)
 
 
+    @commands.is_owner()
     @commands.hybrid_command(name="sync_ctx", description="Sync all commands on all servers (Bot dev only)")
-    @commands.is_owner()
     async def slash_sync_hybrid(self, ctx: commands.Context) -> None:
+        msg = "Synced commands: "
+
         global_sync = await self.bot.tree.sync()
-        guild = self.bot.get_guild(config.getint("Main", "support_guild_id"))
-        local_sync = await self.bot.tree.sync(guild=guild)
-        self.logger.warning(f"{ctx.author} Synced slash commands!")
-        self.logger.debug(f"Synced commands: {global_sync=} {local_sync=}")
         global_list = [command.name for command in global_sync]
-        local_list = [command.name for command in local_sync]
         global_list.sort()
-        local_list.sort()
-        await ctx.send(f"Sync complete\nGlobally synced: {global_list}\nLocally synced: {local_list} to {guild}")
+        msg += f"{global_list}"
+
+        guild = ctx.guild
+
+        local_sync: list[AppCommand] = []
+        for guild in self.bot.guilds:
+            local_sync += await self.bot.tree.sync(guild=guild)
+            local_list = [command.name for command in local_sync]
+            local_list.sort()
+            msg += f" {local_sync} for {guild}"
+
+        self.logger.warning(f"{ctx.author} Synced slash commands!")
+        self.logger.debug(msg)
+        await ctx.send(msg)
 
 
 
