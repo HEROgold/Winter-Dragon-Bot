7ef447133e9907e0cfba68880c913102ac4df55b
HEROgold <martijnwieringa28@gmail.com>
2024-04-08 14:00:27 +0200

    move templates

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 
+from tools.config_reader import TEMPLATE_PATH, config
 from tools.flask_tools import register_blueprints
 
 from .blueprints import ctrl, docs, page
 
 
-app = Flask(__name__)
-
+app = Flask(__name__, template_folder=TEMPLATE_PATH)
+app.logger.setLevel(config["Main"]["log_level"])
 register_blueprints(app, [ctrl, docs, page])
