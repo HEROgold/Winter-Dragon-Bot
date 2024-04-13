a029e48368aa85d60faf0543b436cd03a3eca710
HEROgold <martijnwieringa28@gmail.com>
2024-04-13 20:55:47 +0200

    add logout

--- blueprints/control.py
+++ blueprints/control.py
@@ -1,7 +1,5 @@
 from flask import Blueprint, render_template, request
 
-from tools.database_tables import GuildCommands, Session, UserRoles, engine
-
 
 bp = Blueprint("control", __name__)
 
@@ -9,10 +7,10 @@ bp = Blueprint("control", __name__)
 @bp.route("/settings", methods=["GET", "POST"])
 async def settings() -> str:
     if request.method == "POST":
-        # Update settings in your bot and database based on form data
-        pass
+        return f"{request.args=}"
+    return render_template("settings.j2")
     # Fetch current settings from your bot and database to display in the form
-    with Session(engine) as session:
-        guilds = session.query(GuildCommands).all()
-        users = session.query(UserRoles).all()
-    return render_template("settings.j2", guilds=guilds, users=users)
+    # with Session(engine) as session:
+    #     guilds = session.query(GuildCommands).all()
+    #     users = session.query(UserRoles).all()
+    # return render_template("settings.j2", guilds=guilds, users=users)
