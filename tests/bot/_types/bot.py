e9f70ab4d768a1341acbaaf8f88a5847496aa1c8
HEROgold <martijnwieringa28@gmail.com>
2024-05-15 22:56:08 +0200

    remove random name thing

--- tests/bot/_types/bot.py
+++ tests/bot/_types/bot.py
@@ -8,11 +8,9 @@ from tools.config_reader import INTENTS, config
 
 
 @given(text())
-def test_winter_dragon(name: str) -> None:
+def test_winter_dragon() -> None:
     bot = WinterDragon(
         intents=INTENTS,
         command_prefix=commands.when_mentioned_or(config["Main"]["prefix"]),  # type: ignore
         case_insensitive=True,
     )
-
-    assert bot.name == name
