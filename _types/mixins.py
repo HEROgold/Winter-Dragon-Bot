15f1f60429f848cbdb4f7bb629cf48099edae9c2
HEROgold <martijnwieringa28@gmail.com>
2024-06-23 18:08:16 +0200

    use qualname instead

--- _types/mixins.py
+++ _types/mixins.py
@@ -6,4 +6,4 @@ from config import config
 class LoggerMixin:
     def __init_subclass__(cls, *args, **kwargs) -> None:
         super().__init_subclass__(*args, **kwargs)
-        cls.logger = logging.getLogger(f"{config['Main']['bot_name']}.{cls.__class__.__name__}")
+        cls.logger = logging.getLogger(f"{config['Main']['bot_name']}.{cls.__class__.__qualname__}")
