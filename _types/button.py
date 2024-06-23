437da708f00ba4a070cdfa711e80090d80b11f29
HEROgold <martijnwieringa28@gmail.com>
2024-06-23 18:08:43 +0200

    cleanup

--- _types/button.py
+++ _types/button.py
@@ -1,36 +1,7 @@
-import logging
 
 import discord.ui.button
-from discord.emoji import Emoji
-from discord.enums import ButtonStyle
-from discord.partial_emoji import PartialEmoji
 
-from config import config
+from _types.mixins import LoggerMixin
 
 
-class Button(discord.ui.Button):
-    logger: logging.Logger
-
-    def __init__(  # noqa: PLR0913
-        self,
-        *,
-        style: ButtonStyle = ButtonStyle.secondary,
-        label: str | None = None,
-        disabled: bool = False,
-        custom_id: str | None = None,
-        url: str | None = None,
-        emoji: str | Emoji | PartialEmoji | None = None,
-        row: int | None = None,
-    ) -> None:
-        super().__init__(
-            style=style,
-            label=label,
-            disabled=disabled,
-            custom_id=custom_id,
-            url=url,
-            emoji=emoji,
-            row=row,
-        )
-        self.logger = logging.getLogger(
-            f"{config['Main']['bot_name']}.{self.__class__.__qualname__}",
-        )
+class Button(discord.ui.Button, LoggerMixin): ...
