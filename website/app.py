62ec558cf7e6779ad23088b47c720b799a9fffc6
HEROgold <martijnwieringa28@gmail.com>
2024-07-12 18:02:43 +0200

    add todo

--- website/app.py
+++ website/app.py
@@ -21,8 +21,10 @@ settings = Settings()
 templates = Jinja2Templates(directory="website/templates")
 static = StaticFiles(directory="website/static")
 
+
 def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(secret=settings.SECRET_KEY, lifetime_seconds=3600)
+
 
 bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
 auth_backend = AuthenticationBackend(
@@ -31,16 +33,16 @@ auth_backend = AuthenticationBackend(
     get_strategy=get_jwt_strategy,
 )
 fastapi_users = FastAPIUsers[FastApiUser, int](
-    get_user_manager=get_user_manager(),
+    get_user_manager=get_user_manager(), # TODO: Investigate why this errors.
     auth_backends=[auth_backend],
 )
 
 app.mount("/static", static)
-app.include_router(
-    get_auth_router(auth_backend),
-    prefix="/auth/jwt",
-    targs=["auth"]
-)
+# app.include_router(
+#     get_auth_router(auth_backend),
+#     prefix="/auth/jwt",
+#     targs=["auth"]
+# )
 
 
 @app.get("/items/{id_}", response_class=HTMLResponse)
