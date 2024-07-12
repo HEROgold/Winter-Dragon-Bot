434f13b54a29827d8dc64fe966e225365246f3a5
HEROgold <martijnwieringa28@gmail.com>
2024-07-12 17:49:25 +0200

    fix imports

--- database/tables/__init__.py
+++ database/tables/__init__.py
@@ -1,13 +1,13 @@
 from collections.abc import AsyncGenerator
-from fastapi import Depends
+
 from sqlalchemy import (
     create_engine,
 )
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import (
     DeclarativeBase,
     Session,
 )
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 
 CASCADE = "CASCADE"
