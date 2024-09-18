0860edbd574726b7eb433f27dd291fb6abe0fa1b
HEROgold <martijnwieringa28@gmail.com>
2024-09-18 13:55:54 +0200

    setup

---
+++ bot/extensions/games/league_of_legends.py
@@ -0,0 +1,28 @@
+from bot import WinterDragon
+from bot.types.cogs import Cog
+
+
+class LeagueOfLegends(Cog):
+    """
+    This Cog is for the League of Legends game.
+    It allows users to get information about their games.
+    It can watch and track (ranked) games.
+
+    It can also track the user their:
+    - rank and rank history.
+    - match history.
+    - champion mastery.
+    - champion statistics.
+    - champion leaderboard.
+
+
+    Parameters
+    -----------
+    :param:`Cog`: :class:`_type_`
+        _description_
+    """
+
+
+
+async def setup(bot: WinterDragon) -> None:
+    await bot.add_cog(LeagueOfLegends(bot))
