4ac9624672467719005208d997cf30e95f4f75ad
HEROgold <martijnwieringa28@gmail.com>
2024-10-05 20:29:20 +0200

    allow bigger ints

--- database/tables/role.py
+++ database/tables/role.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String
+from sqlalchemy import BigInteger, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from database.tables.base import Base
@@ -7,5 +7,5 @@ from database.tables.base import Base
 class Role(Base):
     __tablename__ = "roles"
 
-    id: Mapped[int] = mapped_column(primary_key=True, unique=True)
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, unique=True)
     name: Mapped[str] = mapped_column(String)
