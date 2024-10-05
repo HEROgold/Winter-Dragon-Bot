3dadb1f28c8926beb40e688580c2864ca5598761
HEROgold <martijnwieringa28@gmail.com>
2024-10-05 20:42:47 +0200

    set username

--- database/constants.py
+++ database/constants.py
@@ -4,7 +4,7 @@ from sqlalchemy import URL
 CASCADE = "CASCADE"
 DATABASE_URL = URL.create(
     drivername="postgresql",
-    username="aos;idf8p9aoSidUfjAsKdf",
+    username="postgres",
     password="fp9iAsd8ufy7p9UF)p98sduYfjOfd98y123!@3",  # noqa: S106
     host="postgres",
     port=5432,
