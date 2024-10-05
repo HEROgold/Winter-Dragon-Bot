f299ab77a85399a50afb66da8e3c3dfaaf5b15b1
HEROgold <martijnwieringa28@gmail.com>
2024-10-06 01:35:46 +0200

    fix routing

--- website/app.py
+++ website/app.py
@@ -1,4 +1,9 @@
+from api import router as api_router
 from fastapi import FastAPI
+from router import router as global_router
 
 
 app = FastAPI()
+
+app.include_router(global_router)
+app.include_router(api_router)
--- website/main.py
+++ website/main.py
@@ -1,12 +1,9 @@
 import uvicorn
-from api import router as api_router
 from app import app
-from router import router as global_router
 
 
 def main() -> None:
     all_addresses = "0.0.0.0"  # noqa: S104
-    global_router.include_router(api_router)
     uvicorn.run(app, host=all_addresses)
 
 
