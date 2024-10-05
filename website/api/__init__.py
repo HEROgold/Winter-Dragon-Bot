b297e71e6c28ff4d119a5d0f34019fb65293522a
HEROgold <martijnwieringa28@gmail.com>
2024-10-06 00:40:28 +0200

    add api structure

---
+++ website/api/__init__.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+from .admin import router as admin_router
+from .public import router as public_router
+from .user import router as user_router
+
+
+router = APIRouter(prefix="/api", tags=["api"])
+router.include_router(admin_router)
+router.include_router(public_router)
+router.include_router(user_router)
--- website/main.py
+++ website/main.py
@@ -1,9 +1,12 @@
 import uvicorn
+from api import router as api_router
 from app import app
+from router import router as global_router
 
 
 def main() -> None:
     uvicorn.run(app)
+    global_router.include_router(api_router)
 
 
 if __name__ == "__main__":
