f5664e783a27ba24a35bd8a36efa862864e245ba
HEROgold <martijnwieringa28@gmail.com>
2024-10-05 20:28:02 +0200

    create url

--- database/constants.py
+++ database/constants.py
@@ -1,2 +1,12 @@
+from sqlalchemy import URL
+
+
 CASCADE = "CASCADE"
-DATABASE_URL = "sqlite:///database/db.sqlite"
+DATABASE_URL = URL.create(
+    drivername="postgresql",
+    username="aos;idf8p9aoSidUfjAsKdf",
+    password="fp9iAsd8ufy7p9UF)p98sduYfjOfd98y123!@3",  # noqa: S106
+    host="postgres",
+    port=5432,
+    database="winter_dragon",
+)
