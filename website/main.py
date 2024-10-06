097529020fdbbfcf0bdf770ee8d70dd9e9d43600
HEROgold <martijnwieringa28@gmail.com>
2024-10-06 02:31:02 +0200

    remove config and use default https port

--- website/main.py
+++ website/main.py
@@ -1,11 +1,10 @@
 import uvicorn
 from app import app
-from config import config
 
 
 def main() -> None:
     all_addresses = "0.0.0.0"  # noqa: S104
-    uvicorn.run(app, host=all_addresses, port=config.getint("Main", "port"))
+    uvicorn.run(app, host=all_addresses)
 
 
 if __name__ == "__main__":
