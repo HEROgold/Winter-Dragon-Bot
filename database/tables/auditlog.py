a71715e6a569561abc1330a29cd11a14d1599182
HEROgold <martijnwieringa28@gmail.com>
2024-10-06 03:00:05 +0200

    remove runtime dependency

--- database/tables/auditlog.py
+++ database/tables/auditlog.py
@@ -1,28 +1,31 @@
 from datetime import datetime
-from typing import Self
+from typing import TYPE_CHECKING, Self
 
-from discord import AuditLogAction, AuditLogActionCategory, AuditLogEntry
 from sqlalchemy import DateTime, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from database.tables.base import Base
 
 
+if TYPE_CHECKING:
+    from discord import AuditLogEntry
+
+
 class AuditLog(Base):
     __tablename__ = "audit_log"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, unique=True, autoincrement=True)
-    action: Mapped[AuditLogAction] = mapped_column(Integer)
+    action: Mapped[int] = mapped_column(Integer) # AuditLogAction
     reason: Mapped[str] = mapped_column(String(200), nullable=True)
     created_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
     target_id: Mapped[int] = mapped_column(Integer, nullable=True)
-    category: Mapped[AuditLogActionCategory] = mapped_column(Integer, nullable=True)
+    category: Mapped[int] = mapped_column(Integer, nullable=True) # AuditLogActionCategory
     # # changes: Mapped[AuditLogChanges] = mapped_column(, nullable=True)
     # before: Mapped[AuditLogDiff] = mapped_column(String, nullable=True)
     # after: Mapped[AuditLogDiff] = mapped_column(String, nullable=True)
 
     @classmethod
-    def from_audit_log(cls, entry: AuditLogEntry) -> Self:
+    def from_audit_log(cls, entry: "AuditLogEntry") -> Self:
         from database import session
 
         with session:
