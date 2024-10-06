e3c6b24736d0f78f17ee8132bca05720bf55fd18
HEROgold <martijnwieringa28@gmail.com>
2024-10-06 02:45:21 +0200

    add public db interface

--- website/api/public/__init__.py
+++ website/api/public/__init__.py
@@ -1,4 +1,24 @@
 from fastapi import APIRouter
 
+from database import NhieQuestion, Session, WyrQuestion, engine
+
 
 router = APIRouter(tags=["public"])
+
+session = Session(engine)
+
+
+@router.get("/wyr_questions")
+def wyr_questions() -> list[str]:
+    with session:
+        questions: list[WyrQuestion] = []
+        questions.extend(session.query(WyrQuestion).all())
+        return [question.value for question in questions]
+
+
+@router.get("/nhie_questions")
+def nhie_questions() -> list[str]:
+    with session:
+        questions: list[NhieQuestion] = []
+        questions.extend(session.query(NhieQuestion).all())
+        return [question.value for question in questions]
