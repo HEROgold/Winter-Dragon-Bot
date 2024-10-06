def182bb645b88b72f063a6fde140d334f97224b
HEROgold <martijnwieringa28@gmail.com>
2024-10-06 03:09:46 +0200

    remove logging

--- database/tables/user.py
+++ database/tables/user.py
@@ -4,7 +4,6 @@ from sqlalchemy import BigInteger
 from sqlalchemy.orm import Mapped, mapped_column
 
 from database.tables.base import Base
-from tools.main_log import sql_logger as logger
 
 
 class User(Base):
@@ -22,13 +21,9 @@ class User(Base):
         from database import session
 
         with session:
-            logger.debug(f"Looking for user {id_=}")
-
             if user := session.query(cls).where(cls.id == id_).first():
-                logger.debug(f"Returning user {id_=}")
                 return user
 
-            logger.debug(f"Creating user {id_=}")
             session.add(cls(id=id_))
             session.commit()
             return session.query(cls).where(cls.id == id_).first()  # type: ignore
