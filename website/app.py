66ec5161b050422b83921479d362018448abf0bb
HEROgold <martijnwieringa28@gmail.com>
2024-10-10 12:07:12 +0200

    setup a index (needs testing)

--- website/app.py
+++ website/app.py
@@ -1,12 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 
 from website.api import router as api_router
+from website.templates import templates
 
 
 site = FastAPI()
-
 site.mount("/static", StaticFiles(directory="website/static"), name="static")
 
 
 site.include_router(api_router)
+@site.get("/")
+def index(request: Request):
+    head = None
+    header = None
+    nav = None
+    content = None
+    footer = None
+    script = None
+    return templates.TemplateResponse(request, "index.html", {"request": request, "head": head, "header": header, "nav": nav, "content": content, "footer": footer, "script": script})
