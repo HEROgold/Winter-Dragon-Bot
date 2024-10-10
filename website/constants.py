6613d40fa55f2d21b871bd85d7d418b8355beb76
HEROgold <martijnwieringa28@gmail.com>
2024-10-10 22:58:01 +0200

    set app id

--- website/constants.py
+++ website/constants.py
@@ -6,6 +6,7 @@ WEBSITE_DIR = Path(__file__).parent
 WEBSITE_CONFIG = WEBSITE_DIR / "config.ini"
 
 WEBSITE_URL = ""
+APPLICATION_ID = 0
 OAUTH2 = "https://discord.com/api/oauth2"
 DISCORD_AUTHORIZE = f"{OAUTH2}/authorize"
 OAUTH_SCOPE = [
