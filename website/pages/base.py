152318d5c4a8c5f1de6a3d4ccb6ae4019317e472
HEROgold <martijnwieringa28@gmail.com>
2024-10-10 16:51:25 +0200

    add static path

--- website/pages/base.py
+++ website/pages/base.py
@@ -5,6 +5,7 @@ from website.constants import WEBSITE_DIR
 
 TEMPLATES_DIR = WEBSITE_DIR/"templates"
 COMPONENTS_DIR = TEMPLATES_DIR/"components"
+STATIC_DIR = WEBSITE_DIR/"static"
 
 TEMPLATES = Jinja2Templates(directory=TEMPLATES_DIR)
 COMPONENTS = Jinja2Templates(directory=COMPONENTS_DIR)
