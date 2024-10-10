1f39993f9cd9c9e2fab2440d22a393c06ee66c23
HEROgold <martijnwieringa28@gmail.com>
2024-10-10 22:57:49 +0200

    upd

--- website/api/discord_auth.py
+++ website/api/discord_auth.py
@@ -1,18 +1,26 @@
 from fastapi import APIRouter, Request
+from fastapi.responses import RedirectResponse
 
-from website.constants import DISCORD_AUTHORIZE, OAUTH_SCOPE, WEBSITE_URL
+from website.constants import APPLICATION_ID, DISCORD_AUTHORIZE, OAUTH_SCOPE, WEBSITE_URL
 
 
 router = APIRouter(prefix="/oauth", tags=["oauth"])
 
 
-@router.get("/callback")
-def oath_callback(request: Request):
-    # TODO: Respond with the user's home page.
-    return {"message": "Hello, Callback!"}
+@router.get("/")
+def oath(request: Request) -> RedirectResponse:  # noqa: ARG001
+    url = get_oath_url()
+    return RedirectResponse(url=url)
 
-def get_oath_url(application_id: int) -> str:
-    redirect_uri = router.url_path_for("callback")
+
+@router.get("/callback")
+def oath_callback(request: Request) -> dict[str, str]:
+    # TODO: Respond with the user's home page.
+    return {"message": f"{request.__dict__}"}
+
+
+def get_oath_url(application_id: int = APPLICATION_ID) -> str:
+    redirect_uri = router.url_path_for("oauth_callback")
     return (
         DISCORD_AUTHORIZE
         + f"?client_id={application_id}"
