a67c38e5867a8d0a793543bde2e16353d3b35893
HEROgold <martijnwieringa28@gmail.com>
2024-10-10 16:29:40 +0200

    move to own file

--- website/app.py
+++ website/app.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 from website.api import router as api_router
-from website.templates import footer, head, header, nav, templates
+from website.pages import router as pages_router
 
 
 site = FastAPI()
@@ -10,20 +10,3 @@ site.mount("/static", StaticFiles(directory="website/static"), name="static")
 
 
 site.include_router(api_router)
-
-
-@site.get("/")
-def index(request: Request):
-    return templates.TemplateResponse(
-        request,
-        "index.j2",
-        {
-            "request": request,
-            "head": head,
-            "header": header,
-            "nav": nav,
-            "content": """content""",
-            "footer": footer,
-            "script": """script""",
-        },
-    )
