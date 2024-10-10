b0f4273c2f705ea86dcd21b2e7fdc90f5c52a1f6
HEROgold <martijnwieringa28@gmail.com>
2024-10-10 13:30:01 +0200

    some more templating (needs fixing)

--- website/app.py
+++ website/app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 
 from website.api import router as api_router
-from website.templates import templates
+from website.templates import footer, head, header, nav, templates
 
 
 site = FastAPI()
@@ -10,12 +10,20 @@ site.mount("/static", StaticFiles(directory="website/static"), name="static")
 
 
 site.include_router(api_router)
+
+
 @site.get("/")
 def index(request: Request):
-    head = None
-    header = None
-    nav = None
-    content = None
-    footer = None
-    script = None
-    return templates.TemplateResponse(request, "index.html", {"request": request, "head": head, "header": header, "nav": nav, "content": content, "footer": footer, "script": script})
+    return templates.TemplateResponse(
+        request,
+        "index.j2",
+        {
+            "request": request,
+            "head": head,
+            "header": header,
+            "nav": nav,
+            "content": """content""",
+            "footer": footer,
+            "script": """script""",
+        },
+    )
--- website/pages/base.py
+++ website/pages/base.py
@@ -3,5 +3,8 @@ from fastapi.templating import Jinja2Templates
 from website.constants import WEBSITE_DIR
 
 
-COMPONENTS = Jinja2Templates(directory=WEBSITE_DIR/"components")
-TEMPLATES = Jinja2Templates(directory=WEBSITE_DIR/"templates")
+TEMPLATES_DIR = WEBSITE_DIR/"templates"
+COMPONENTS_DIR = TEMPLATES_DIR/"components"
+
+TEMPLATES = Jinja2Templates(directory=TEMPLATES_DIR)
+COMPONENTS = Jinja2Templates(directory=COMPONENTS_DIR)
