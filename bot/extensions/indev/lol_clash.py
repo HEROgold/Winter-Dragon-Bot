e764127df7169158c8bf07934c3507b8f8995fe7
HEROgold <martijnwieringa28@gmail.com>
2024-10-19 18:50:28 +0200

    add

---
+++ bot/extensions/indev/lol_clash.py
@@ -0,0 +1,6 @@
+"""
+Contains the Clash cog for the bot.
+
+Allows for automatic event reminders for the Clash event in League of Legends.
+Allows for LFG (Looking For Group) for the Clash event in League of Legends.
+"""
