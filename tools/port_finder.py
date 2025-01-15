1287d0ecdb894948677399b22af9d6978c109ca3
HEROgold <martijnwieringa28@gmail.com>
2025-01-15 18:23:20 +0100

    update

--- tools/port_finder.py
+++ tools/port_finder.py
@@ -1,10 +1,8 @@
 import socket
 
 
-v4_port = 5000
-
 def get_v4_port() -> int:
-    """Get a random available v4 port, or the port in use.
+    """Get an available v4 port, or the port in use.
 
     Returns
     -------
@@ -12,13 +10,6 @@ def get_v4_port() -> int:
         the port found, or in use
 
     """
-    # FIXME @<HEROgold>: A randomized port doesn't work with oath2 requests, as discord requires a static port.
-    # 130
-    # This is why the v4_port is set to 5000 manually.
-    global v4_port
-    if v4_port != 0:
-        return v4_port
-
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind(("127.0.0.1", 0))
     _, v4_port = sock.getsockname()
