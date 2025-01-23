1bb34f083be7da65b11975a1e95e6f1818e2a40a
HEROgold <martijnwieringa28@gmail.com>
2025-01-24 00:23:15 +0100

    add empty favicon

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,8 +1,13 @@
 from api import router as api_router
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 
 site = FastAPI()
 
 
 site.include_router(api_router)
+
+@site.get("/favicon.ico")
+def favicon() -> FileResponse:
+    return FileResponse("static/favicon.ico")
