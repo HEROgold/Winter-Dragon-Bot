cd8de40206968b674781c15d5285f435932a34e7
HEROgold <martijnwieringa28@gmail.com>
2025-02-18 21:53:50 +0100

    add embed display

--- bot/src/extensions/games/incremental.py
+++ bot/src/extensions/games/incremental.py
@@ -15,7 +15,9 @@ from database.tables.incremental.rates import GeneratorRates
 
 
 type Player = discord.User | discord.Member
-
+# TODO @HEROgold: Use a better type for resources.
+# 0
+type IncrementalRecources = dict[str, int]
 
 class Incremental(GroupCog):
     def init_player(self, user: Player) -> None:
@@ -50,8 +52,13 @@ class Incremental(GroupCog):
     def update_collection_time(self, player: IncrementalPlayer, time_stamp: datetime) -> None:
         player.last_collection = time_stamp
 
+    def get_resources(self, player: Player) -> IncrementalRecources:
+        with self.session as session:
+            balance = session.query(UserMoney).where(UserMoney.user_id == player.id).first()
+            return {"Balance": balance.value}
+
     def add_resources(self, player: Player) -> None:
-        # Add resources to player's inventory.
+        """Add resources to player's inventory based on time difference."""
         time = self.get_time_difference(player)
         generators = self.get_generators(player)
         with self.session as session:
@@ -66,14 +73,32 @@ class Incremental(GroupCog):
         # TODO @HEROgold: return a embed containing the resources collected.
         # 000
 
+    def generate_embed(self, player: Player, resources: IncrementalRecources) -> discord.Embed:
+        with self.session as session:
+            balance = session.query(UserMoney).where(UserMoney.user_id == player.id).first()
+            embed = discord.Embed(title="Resources Collected", color=discord.Color.green())
+            for k, v in resources.items():
+                embed.add_field(name=k, value=v, inline=False)
+            embed.set_footer(text=f"Total Balance: {balance.value}")
+            return embed
+
     async def slash_collect(self, interaction: discord.Interaction) -> None:
         # Collect resources
         # Get player's generators, calculate resources, and add them to player's inventory.
         player = interaction.user
         self.init_player(player)
 
+        old = self.get_resources(player)
         self.add_resources(player)
-        await interaction.response.send_message("Resources collected!", ephemeral=True)
+        new = self.get_resources(player)
+        difference = {k: new[k] - old[k] for k in old}
+
+        self.generate_embed(player, difference)
+        await interaction.response.send_message(
+            "Resources collected!",
+            ephemeral=True,
+            embed=self.generate_embed(player, difference),
+        )
 
 
 async def setup(bot: WinterDragon) -> None:
