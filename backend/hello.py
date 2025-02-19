34829f35087267b72857f48184dbbd56661e8434
HEROgold <martijnwieringa28@gmail.com>
2025-02-19 15:52:52 +0100

    sort

--- backend/hello.py
+++ backend/hello.py
@@ -1,5 +1,5 @@
-def main():
-    print("Hello from backend!")
+def main() -> None:
+    pass
 
 
 if __name__ == "__main__":
--- bot/hello.py
+++ bot/hello.py
@@ -1,5 +1,5 @@
-def main():
-    print("Hello from bot!")
+def main() -> None:
+    pass
 
 
 if __name__ == "__main__":
