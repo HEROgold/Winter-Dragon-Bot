c0fa373fd77ad82507bc895a376d33473f2037d9
HEROgold <martijnwieringa28@gmail.com>
2025-02-24 16:51:29 +0100

    fix requirement

--- database/tables/lobby.py
+++ database/tables/lobby.py
@@ -6,5 +6,5 @@ from database.tables.definitions import GAMES_NAME, LOBBY_STATUS
 class Lobby(SQLModel, table=True):
 
     id: int | None = Field(default=None, primary_key=True)
-    game: str = Field(foreign_key=GAMES_NAME)
+    game: str | None = Field(foreign_key=GAMES_NAME, nullable=True)
     status: str = Field(foreign_key=LOBBY_STATUS)
