2851afa207b4bf90d6fbc95e60edbff7af075e5d
HEROgold <martijnwieringa28@gmail.com>
2025-03-19 23:20:18 +0100

    move and rename logging module

--- bot/core/log.py
+++ bot/core/log.py
@@ -1,3 +1,4 @@
+"""Module for WinterDragon loggers."""
 import logging
 from typing import Any
 
@@ -5,10 +6,13 @@ from config import config
 
 
 class LoggerMixin:
+    """Mixin class for adding loggers to classes based on their names."""
+
     logger: logging.Logger
     _furthest_child_name = None  # Track the name of the furthest child class
 
     def __init_subclass__(cls, **kwargs: Any) -> None:  # noqa: ANN401
+        """Initialize subclasses. Adding loggers to the class with names based on the class name."""
         super().__init_subclass__(**kwargs)
         # Update the furthest child name with the current subclass name
         LoggerMixin._furthest_child_name = cls.__name__
@@ -17,6 +21,7 @@ class LoggerMixin:
 
     @classmethod
     def update_logger_for_hierarchy(cls) -> None:
+        """Update the logger for all classes in the mro hierarchy."""
         # Traverse the MRO (Method Resolution Order) in reverse to update logger from base to the furthest child
         for base in reversed(cls.mro()):
             if issubclass(base, LoggerMixin) and hasattr(base, "_furthest_child_name"):
