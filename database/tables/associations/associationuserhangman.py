ebe2a67904a04edd5ea1916f5a767db4c4d8ff8b
HEROgold <martijnwieringa28@gmail.com>
2025-03-19 23:22:23 +0100

    typing

--- database/tables/associations/associationuserhangman.py
+++ database/tables/associations/associationuserhangman.py
@@ -6,6 +6,6 @@ from database.tables.definitions import AUTO_INCREMENT_ID, HANGMEN_ID, USERS_ID
 class AssociationUserHangman(SQLModel, table=True):
 
     id = AUTO_INCREMENT_ID
-    hangman_id = Field(foreign_key=HANGMEN_ID)
+    hangman_id: int = Field(foreign_key=HANGMEN_ID)
     user_id: int = Field(foreign_key=USERS_ID)
     score: int
