080e20a8ae5530e57e8863b5fc8b6bd4ee0bef1c
HEROgold <martijnwieringa28@gmail.com>
2025-04-20 12:07:20 +0200

    add relationships

--- database/src/winter_dragon/database/tables/associations/associationusercommand.py
+++ database/src/winter_dragon/database/tables/associations/associationusercommand.py
@@ -1,5 +1,5 @@
 from sqlalchemy import func
-from sqlmodel import Field, SQLModel, select
+from sqlmodel import Field, Relationship, SQLModel, select
 from winter_dragon.database.keys import get_foreign_key
 from winter_dragon.database.tables.command import Commands
 from winter_dragon.database.tables.user import Users
@@ -9,6 +9,9 @@ class AssociationUserCommand(SQLModel, table=True):
 
     user_id: int = Field(foreign_key=get_foreign_key(Users, "id"), primary_key=True)
     command_id: int = Field(foreign_key=get_foreign_key(Commands, "id"), primary_key=True)
+
+    user: Users = Relationship()
+    command: Commands = Relationship()
 
     @classmethod
     def cleanup(cls) -> None:  # sourcery skip: remove-unreachable-code
