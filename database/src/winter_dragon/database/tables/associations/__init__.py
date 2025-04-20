aa31522981aa681f5dcd544819d66674ecb95298
HEROgold <martijnwieringa28@gmail.com>
2025-04-20 12:28:32 +0200

    sort

--- database/src/winter_dragon/database/tables/associations/__init__.py
+++ database/src/winter_dragon/database/tables/associations/__init__.py
@@ -1,8 +1,8 @@
+from .guild_commands import GuildCommands
+from .guild_roles import GuildRoles
 from .user_command import AssociationUserCommand
 from .user_hangman import AssociationUserHangman
 from .user_lobby import AssociationUserLobby
-from .guild_commands import GuildCommands
-from .guild_roles import GuildRoles
 
 
 __all__ = [
