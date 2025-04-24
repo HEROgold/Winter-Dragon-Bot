78928c88527bc44066aa3dc0daee55e4da79c944
HEROgold <martijnwieringa28@gmail.com>
2025-04-24 16:35:41 +0200

    refactor the stat channels to be seperated into their own classes, with their own update methods.

--- bot/src/winter_dragon/bot/extensions/server/stats.py
+++ bot/src/winter_dragon/bot/extensions/server/stats.py
@@ -1,12 +1,17 @@
+"""Module that contains relevant classes to display stats about the guild."""
 import random
+from abc import ABC, ABCMeta, abstractmethod
+from collections.abc import Generator
+from typing import TYPE_CHECKING, NoReturn
 
 import discord
-from discord import app_commands
+from discord import Guild, VoiceChannel, app_commands
 from discord.ext import commands
 from sqlmodel import select
 from winter_dragon.bot.config import config
 from winter_dragon.bot.core.bot import WinterDragon
 from winter_dragon.bot.core.cogs import Cog, GroupCog
+from winter_dragon.bot.core.log import LoggerMixin
 from winter_dragon.bot.core.tasks import loop
 from winter_dragon.bot.enums.channels import ChannelTypes
 from winter_dragon.bot.errors import NoneTypeError
@@ -14,7 +19,11 @@ from winter_dragon.bot.tools import rainbow
 from winter_dragon.database.tables import Channels
 
 
-STATS = ChannelTypes.STATS.name
+if TYPE_CHECKING:
+    from winter_dragon.bot._types.aliases import PermissionsOverwrites
+
+
+STATS = ChannelTypes.STATS
 
 
 def get_peak_count(channel: Channels | discord.abc.GuildChannel) -> int:
@@ -25,10 +34,174 @@ def get_peak_count(channel: Channels | discord.abc.GuildChannel) -> int:
         return 0
 
 
+class StatChannel(LoggerMixin, ABC, metaclass=ABCMeta):
+    """Base class for all stat channels."""
+
+    update_reason = "Updating Stats channels"
+
+    def __init__(self, channel: VoiceChannel | None) -> None:
+        """Initialize the stat channel."""
+        self.channel = channel
+
+    @abstractmethod
+    async def update(self) -> None: ...  # noqa: D102
+
+class PeakStat(StatChannel):
+    """Class for the peak stat channel."""
+
+    @property
+    def peak_count(self) -> int:
+        """Get the peak count from the channel name."""
+        return 0 if self.channel is None else get_peak_count(self.channel)
+
+    @property
+    def currently_online(self) -> int:
+        """Get the current number of online members."""
+        if self.channel is None:
+            return 0
+        return sum(member.status != discord.Status.offline and not member.bot for member in self.channel.guild.members)
+
+    async def update(self) -> None:
+        """Update the peak channel name to display the current peak."""
+        if self.channel is None:
+            return
+        if self.peak_count >= self.currently_online:
+            return
+        new_name = f"Peak Online: {self.peak_count}"
+        if new_name == self.channel.name:
+            return
+        self.logger.debug(f"Updating peak channel name: {self.channel.name} -> {new_name}")
+        await self.channel.edit(name=new_name, reason=self.update_reason)
+
+class GuildStat(StatChannel):
+    """Class for the guild creation date channel."""
+
+    async def update(self) -> None:
+        """Update the guild channel name to display the creation date."""
+        if self.channel is None:
+            return
+        age = self.channel.guild.created_at.strftime("%Y-%m-%d")
+        new_name = f"Created On: {age}"
+        if new_name == self.channel.name:
+            return
+        self.logger.debug(f"Updating guild channel name: {self.channel.name} -> {new_name}")
+        await self.channel.edit(name=new_name, reason=self.update_reason)
+
+class BotStat(StatChannel):
+    """Class for the bot stat channel."""
+
+    async def update(self) -> None:
+        """Update the bot channel name to display the number of bots."""
+        if self.channel is None:
+            return
+        bot_count = sum(member.bot is True for member in self.channel.guild.members)
+        new_name = f"Total Bots: {bot_count}"
+        if new_name == self.channel.name:
+            return
+        self.logger.debug(f"Updating bot channel name: {self.channel.name} -> {new_name}")
+        await self.channel.edit(name=new_name, reason=self.update_reason)
+
+class UserStat(StatChannel):
+    """Class for the user stat channel."""
+
+    @property
+    def user_count(self) -> int:
+        """Get the number of users in the channel.
+
+        Returns 0 if the channel is None.
+        """
+        if self.channel is None:
+            return 0
+        return sum(member.bot is False for member in self.channel.members)
+
+    async def update(self) -> None:
+        """Update the user channel name to display the number of users."""
+        if self.channel is None:
+            return
+        new_name = f"Total Users: {self.user_count}"
+        if new_name == self.channel.name:
+            return
+        self.logger.debug(f"Updating user channel name: {self.channel.name} -> {new_name}")
+        await self.channel.edit(name=new_name, reason=self.update_reason)
+
+class OnlineStat(StatChannel):
+    """Class for the online stat channel."""
+
+    @property
+    def online_count(self) -> int:
+        """Get the number of online users in the channel.
+
+        Returns 0 if the channel is None.
+        """
+        if self.channel is None:
+            return 0
+        return sum(member.status != discord.Status.offline and not member.bot for member in self.channel.members)
+
+    async def update(self) -> None:
+        """Update the online channel name to display the number of online users."""
+        if self.channel is None:
+            return
+        new_name = f"Online Users: {self.online_count}"
+        if new_name == self.channel.name:
+            return
+        self.logger.debug(f"Updating online channel name: {self.channel.name} -> {new_name}")
+        await self.channel.edit(name=new_name, reason=self.update_reason)
+
+
+
+class StatChannels:
+    """Container for all stat channels related to one guild."""
+
+    __slots__ = (
+        "bot_channel",
+        "guild_channel",
+        "online_channel",
+        "peak_channel",
+        "user_channel",
+    )
+
+    def __init__(
+        self,
+        peak_channel: PeakStat | None = None,
+        guild_channel: GuildStat | None = None,
+        bot_channel: BotStat | None = None,
+        user_channel: UserStat | None = None,
+        online_channel: OnlineStat | None = None,
+    ) -> None:
+        """Initialize the stat channels."""
+        self.peak_channel = peak_channel
+        self.guild_channel = guild_channel
+        self.bot_channel = bot_channel
+        self.user_channel = user_channel
+        self.online_channel = online_channel
+
+    def __iter__(self) -> Generator[StatChannel, None, NoReturn]:
+        """Return all stat channels."""
+        for channel in self._get_channels():
+            if channel:
+                yield channel
+        raise StopIteration
+
+    def _get_channels(self) -> tuple[StatChannel | None, ...]:
+        return (self.peak_channel, self.guild_channel, self.bot_channel, self.user_channel, self.online_channel)
+
+
+
 @app_commands.guild_only()
 class Stats(GroupCog):
+    """Cog that contains all guild stats related commands."""
+
+    def __init__(self, *args: WinterDragon, **kwargs: WinterDragon) -> None:
+        """Initialize the stats cog."""
+        super().__init__(*args, **kwargs)
+        self.stat_channels: dict[Guild, StatChannels] = {
+            i: StatChannels(*self.get_guild_stats_channels(i))
+            for i in self.bot.guilds
+        }
+
     @Cog.listener()
     async def on_member_update(self, before: discord.Member, after: discord.Member) -> None:
+        """Update the appropriate stats channels when a member update is fired."""
         member = before or after
         self.logger.debug(f"Member update: {member.guild=}, {member=}")
         guild = member.guild
@@ -41,49 +214,42 @@ class Stats(GroupCog):
                     ),
                 ).first()
             ):
-                await self.update_peak(guild, peak_online)
-
-    async def update_peak(self, guild: discord.Guild, peak_online: Channels) -> None:
-        peak_channel_id = peak_online.id
-        peak_channel = guild.get_channel(peak_channel_id)
-        if peak_channel is None:
-            self.logger.warning(f"Peak channel not found: {peak_online}")
-            return
-
-        peak_count = get_peak_count(peak_channel)
-        online = sum(member.status != discord.Status.offline and not member.bot for member in guild.members)
-        if online > peak_count:
-            await peak_channel.edit(name=f"Peak Online: {peak_count}", reason="Reached new peak of online members")
-            self.logger.info(f"New peak online reached for {guild}!")
+                peak_channel = guild.get_channel(peak_online.id)
+                if not isinstance(peak_channel, VoiceChannel):
+                    msg = f"Expected VoiceChannel during stat channel update, got {type(peak_channel)}"
+                    self.logger.warning(msg)
+                    return
+                await PeakStat(peak_channel).update()
 
     async def create_stats_channels(
         self,
         guild: discord.Guild | None,
         reason: str | None = None,
     ) -> None:
+        """Create all stats channels for a guild."""
         if guild is None:
             msg = "Expected discord.Guild"
             raise NoneTypeError(msg)
 
-        overwrite = {
+        overwrite: PermissionsOverwrites = {
             guild.default_role: discord.PermissionOverwrite(connect=False, view_channel=True),
             guild.me: discord.PermissionOverwrite(connect=True),
         }
 
-        category = await guild.create_category(name=STATS, overwrites=overwrite, position=0)
+        category = await guild.create_category(name=STATS.name, overwrites=overwrite, position=0)
         user_channel = await category.create_voice_channel(name="Total Users:", reason=reason)
         online_channel = await category.create_voice_channel(name="Online Users:", reason=reason)
         bot_channel = await category.create_voice_channel(name="Total Bots:", reason=reason)
-        peak_channel = await category.create_voice_channel(name="Peak Online:", reason=reason)
         guild_channel = await category.create_voice_channel(name="Created On:", reason=reason)
+        peak_channel = await category.create_voice_channel(name="Peak Online:", reason=reason)
 
         channels: dict[str, discord.abc.GuildChannel] = {
-            "online_channel": online_channel,
+            "category_channel": category,
             "user_channel": user_channel,
+            "online_channel": online_channel,
             "bot_channel": bot_channel,
             "guild_channel": guild_channel,
             "peak_channel": peak_channel,
-            "category_channel": category,
         }
 
         with self.session as session:
@@ -104,8 +270,10 @@ class Stats(GroupCog):
         guild: discord.Guild | None,
         reason: str | None = None,
     ) -> None:
+        """Remove all stats channels for a guild."""
         if guild is None:
-            NoneTypeError("Expected discord.guild")
+            msg = "Expected discord.guild"
+            raise NoneTypeError(msg)
 
         with self.session as session:
             channels = session.exec(
@@ -121,8 +289,8 @@ class Stats(GroupCog):
             for db_channel in channels:
                 self.logger.debug(f"removing {db_channel}")
                 try:
-                    channel = discord.utils.get(guild.channels, id=db_channel.id)
-                    await channel.delete(reason=reason)
+                    if channel := discord.utils.get(guild.channels, id=db_channel.id):
+                        await channel.delete(reason=reason)
                 except AttributeError:
                     self.logger.exception("")
                 finally:
@@ -130,10 +298,14 @@ class Stats(GroupCog):
             session.commit()
 
     async def cog_load(self) -> None:
+        """Load the cog."""
         self.update.start()
 
+    # TODO: test the line below, and set a default to 3600
+    # @Config.as_kwarg("stats", "update_interval", "seconds")
     @loop(seconds=3600)
     async def update(self) -> None:
+        """Update all stat channels periodically."""
         # Note: keep for loop with if.
         # because fetching guild won't let the bot get members from guild.
         self.logger.info("Updating all stat channels")
@@ -148,48 +320,27 @@ class Stats(GroupCog):
                 ).first():
                     continue
             self.logger.info(f"Updating stat channels: guild='{guild}'")
-
-            peak_channel, guild_channel, bot_channel, user_channel, online_channel = await self.get_guild_stats_channels(guild)
-            self.logger.debug(
-                f"Channels list: {peak_channel}, {user_channel}, {bot_channel}, {online_channel}, {guild_channel}",
-            )
-
-            peak_count = get_peak_count(peak_channel)
-            reason_update = "Updating Stats channels"
-            online = sum(member.status != discord.Status.offline and not member.bot for member in guild.members)
-            users = sum(member.bot is False for member in guild.members)
-            bots = sum(member.bot is True for member in guild.members)
-            age = guild.created_at.strftime("%Y-%m-%d")
-            peak_online = max(online, peak_count)
-
-            if (new_name := f"Online Users: {online}") != online_channel.name:
-                await online_channel.edit(name=new_name, reason=reason_update)
-
-            if (new_name := f"Total Users: {users}") != user_channel.name:
-                await user_channel.edit(name=new_name, reason=reason_update)
-
-            if (new_name := f"Total Bots: {bots}") != bot_channel.name:
-                await bot_channel.edit(name=new_name, reason=reason_update)
-
-            if (new_name := f"Created On: {age}") != guild_channel.name:
-                await guild_channel.edit(name=new_name, reason=reason_update)
-
-            if (new_name := f"Peak Online: {peak_online}") != peak_channel.name:
-                await peak_channel.edit(name=new_name, reason=reason_update)
-
-            self.logger.debug(f"Channels Stats: {peak_online},{users}, {bots}, {online},  {age}, {len(list(guild.members))}")
+            self.stat_channels[guild] = StatChannels(*self.get_guild_stats_channels(guild))
+            for channel in self.stat_channels[guild]:
+                await channel.update()
             self.logger.info(f"Updated stat channels: guild='{guild}'")
 
-    async def get_guild_stats_channels(
+    def get_guild_stats_channels(
         self,
         guild: discord.Guild,
     ) -> tuple[
-        discord.abc.GuildChannel | None,
-        discord.abc.GuildChannel | None,
-        discord.abc.GuildChannel | None,
-        discord.abc.GuildChannel | None,
-        discord.abc.GuildChannel | None,
+        PeakStat | None,
+        GuildStat | None,
+        BotStat | None,
+        UserStat | None,
+        OnlineStat | None,
     ]:
+        """Get all stat channels for a guild."""
+        user_channel = None
+        online_channel = None
+        bot_channel = None
+        peak_channel = None
+        guild_channel = None
         with self.session as session:
             channels = session.exec(
                 select(Channels).where(
@@ -199,21 +350,18 @@ class Stats(GroupCog):
             ).all()
 
             for channel in channels:
-                if channel.id is None:
-                    self.logger.warning(f"Channel id is None: {channel=}")
-                    continue
                 self.logger.debug(f"check if stats channel: {channel=}")
                 match channel.name:
                     case "user_channel":
-                        user_channel = guild.get_channel(channel.id)
+                        user_channel = UserStat(discord.utils.get(guild.voice_channels, id=channel.id))
                     case "online_channel":
-                        online_channel = guild.get_channel(channel.id)
+                        online_channel = OnlineStat(discord.utils.get(guild.voice_channels, id=channel.id))
                     case "bot_channel":
-                        bot_channel = guild.get_channel(channel.id)
+                        bot_channel = BotStat(discord.utils.get(guild.voice_channels, id=channel.id))
                     case "peak_channel":
-                        peak_channel = guild.get_channel(channel.id)
+                        peak_channel = PeakStat(discord.utils.get(guild.voice_channels, id=channel.id))
                     case "guild_channel":
-                        guild_channel = guild.get_channel(channel.id)
+                        guild_channel = GuildStat(discord.utils.get(guild.voice_channels, id=channel.id))
                     case "category_channel" | "stats":
                         continue
                     case _:
@@ -225,7 +373,15 @@ class Stats(GroupCog):
 
     @app_commands.command(name="show", description="Get some information about the guild!")
     async def slash_stats_show(self, interaction: discord.Interaction) -> None:
+        """Show some stats about the guild."""
         guild = interaction.guild
+        if guild is None:
+            msg = "Expected Guild"
+            raise NoneTypeError(msg)
+        if guild.afk_channel is None:
+            msg = "No afk channel found"
+            raise NoneTypeError(msg)
+
         embed = discord.Embed(
             title=f"{guild.name} Stats",
             description=f"Information about {guild.name}",
@@ -275,6 +431,11 @@ class Stats(GroupCog):
         description="This command will create the Stats category which will show some stats about the guild.",
     )
     async def slash_stats_category_add(self, interaction: discord.Interaction) -> None:
+        """Create stat channels."""
+        if interaction.guild is None:
+            msg = "Expected Guild"
+            raise NoneTypeError(msg)
+
         with self.session as session:
             if session.exec(
                 select(Channels).where(
@@ -305,6 +466,11 @@ class Stats(GroupCog):
     @app_commands.checks.has_permissions(manage_channels=True)
     @app_commands.checks.bot_has_permissions(manage_channels=True)
     async def slash_stats_category_remove(self, interaction: discord.Interaction) -> None:
+        """Remove stat channels."""
+        if interaction.guild is None:
+            msg = "Expected Guild"
+            raise NoneTypeError(msg)
+
         with self.session as session:
             channels = session.exec(
                 select(Channels).where(
@@ -333,6 +499,11 @@ class Stats(GroupCog):
     @app_commands.guilds(config.getint("Main", "support_guild_id"))
     @commands.is_owner()
     async def reset_stats(self, interaction: discord.Interaction) -> None:
+        """Reset all stats on the stat channels."""
+        if interaction.guild is None:
+            msg = "Expected Guild"
+            raise NoneTypeError(msg)
+
         self.logger.warning(f"Resetting all guild/stats channels > by: {interaction.user}")
         await interaction.response.defer(ephemeral=True)
 
