26f4dbb90e586917e8a126b8bf5cd8a434fc8620
HEROgold <martijnwieringa28@gmail.com>
2025-04-25 17:18:32 +0200

    use bigint from Users

--- database/src/winter_dragon/database/tables/steamuser.py
+++ database/src/winter_dragon/database/tables/steamuser.py
@@ -1,3 +1,4 @@
+from sqlalchemy import Column, ForeignKey
 from sqlmodel import Field, SQLModel
 from winter_dragon.database.keys import get_foreign_key
 from winter_dragon.database.tables.user import Users
@@ -5,4 +6,4 @@ from winter_dragon.database.tables.user import Users
 
 class SteamUsers(SQLModel, table=True):
 
-    id: int = Field(foreign_key=get_foreign_key(Users, "id"), primary_key=True)
+    id: int = Field(sa_column=Column(ForeignKey(get_foreign_key(Users, "id"))))
