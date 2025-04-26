3a26ee182b036142854931ced760d48bff29d638
HEROgold <martijnwieringa28@gmail.com>
2025-04-26 15:39:51 +0200

    add udpate method

--- database/src/winter_dragon/database/tables/steamsale.py
+++ database/src/winter_dragon/database/tables/steamsale.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, Session, SQLModel, select
 
 
 class SteamSale(SQLModel, table=True):
@@ -13,3 +13,30 @@ class SteamSale(SQLModel, table=True):
     is_dlc: bool
     is_bundle: bool
     update_datetime: datetime
+
+
+    def update(self, session: Session) -> bool:
+        """Update/override a sale record in Database.
+
+        Args:
+        ----
+            session (Session): Session to connect to DataBase
+            sale (SteamSale): Sale to update
+
+        Returns:
+        -------
+            bool: True when updated, False when not updated
+
+        """
+        if known := session.exec(select(SteamSale).where(SteamSale.id == self.id)).first():
+            known.title = self.title
+            known.url = self.url
+            known.sale_percent = self.sale_percent
+            known.final_price = self.final_price
+            known.is_dlc = self.is_dlc
+            known.is_bundle = self.is_bundle
+            known.update_datetime = self.update_datetime
+            session.add(known)
+            session.commit()
+            return True
+        return False
