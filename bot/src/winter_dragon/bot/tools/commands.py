c8321881670b216063e252a53cb6bf8c75d12825
HEROgold <martijnwieringa28@gmail.com>
2025-05-02 15:02:20 +0200

    update message on error

--- bot/src/winter_dragon/bot/tools/commands.py
+++ bot/src/winter_dragon/bot/tools/commands.py
@@ -12,11 +12,11 @@ class Commands(LoggerMixin):
     def get_command_mention(bot: WinterDragon, command: app_commands.Command) -> str:
         """Return a command string from a given functiontype. (Decorated with app_commands.command)."""
         if not isinstance(command, app_commands.Command):  # type:ignore[reportUnnecessaryIsInstance]
-            msg = f"Expected app_commands.commands.Command but got {type(command)} instead"
+            msg = f"Expected app_commands.commands.Command but got {type(command)}"
             raise TypeError(msg)
 
         if cmd := bot.get_app_command(command.qualified_name):
             return cmd.mention
-        msg = f"Can't find {command}"
-        Commands.logger.warning(msg)
+        msg = f"Can't find {command.qualified_name}"
+        Commands.logger.exception(msg)
         raise ValueError(msg)
