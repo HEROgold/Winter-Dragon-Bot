4e68744ff552544595af0cd9fb70faf121cc68e0
HEROgold <martijnwieringa28@gmail.com>
2025-05-03 14:57:13 +0200

    seperate validation and getting commands

--- bot/src/winter_dragon/bot/tools/commands.py
+++ bot/src/winter_dragon/bot/tools/commands.py
@@ -9,14 +9,24 @@ class Commands(LoggerMixin):
     """Utility tools for commands."""
 
     @staticmethod
-    def get_command_mention(bot: WinterDragon, command: app_commands.Command) -> str:
-        """Return a command string from a given functiontype. (Decorated with app_commands.command)."""
-        if not isinstance(command, app_commands.Command):  # type:ignore[reportUnnecessaryIsInstance]
-            msg = f"Expected app_commands.commands.Command but got {type(command)}"
-            raise TypeError(msg)
+    def _get_cmd_mention(bot: WinterDragon, command: app_commands.Command | str) -> str:
+        name = command if isinstance(command, str) else command.qualified_name
 
-        if cmd := bot.get_app_command(command.qualified_name):
+        if cmd := bot.get_app_command(name):
             return cmd.mention
-        msg = f"Can't find {command.qualified_name}"
+        msg = f"Can't find {name}"
         Commands.logger.exception(msg)
         raise ValueError(msg)
+
+    @staticmethod
+    def _validate_command(command: app_commands.Command | str) -> None:
+        """Validate the command type."""
+        if not isinstance(command, (app_commands.Command, str)):  # type:ignore[reportUnnecessaryIsInstance]
+            msg = f"Expected `app_commands.commands.Command` or `str` but got {type(command)}"
+            raise TypeError(msg)
+
+    @staticmethod
+    def get_command_mention(bot: WinterDragon, command: app_commands.Command | str) -> str:
+        """Return a command string from a given functiontype. (Decorated with app_commands.command)."""
+        Commands._validate_command(command)
+        return Commands._get_cmd_mention(bot, command)
