d9121e9af709b78b99c2d806d3a857c415a8eafe
HEROgold <martijnwieringa28@gmail.com>
2025-05-03 15:02:04 +0200

    fix table int size

--- database/src/winter_dragon/database/tables/message.py
+++ database/src/winter_dragon/database/tables/message.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey
+from sqlalchemy import BigInteger, Column, ForeignKey
 from sqlmodel import Field, SQLModel
 from winter_dragon.database.keys import get_foreign_key
 from winter_dragon.database.tables.channel import Channels
@@ -7,7 +7,7 @@ from winter_dragon.database.tables.user import Users
 
 class Messages(SQLModel, table=True):
 
-    id: int = Field(primary_key=True, unique=True)
+    id: int = Field(sa_column=Column(BigInteger(), primary_key=True))
     content: str
     user_id: int = Field(sa_column=Column(ForeignKey(get_foreign_key(Users, "id"))))
     channel_id: int = Field(sa_column=Column(ForeignKey(get_foreign_key(Channels, "id")), nullable=True))
