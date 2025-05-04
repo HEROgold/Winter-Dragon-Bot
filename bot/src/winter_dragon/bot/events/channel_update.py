cfbf3568e9413589ee2573a3d07429aedd105586
HEROgold <martijnwieringa28@gmail.com>
2025-05-04 14:20:16 +0200

    fix update to use helper function

--- bot/src/winter_dragon/bot/events/channel_update.py
+++ bot/src/winter_dragon/bot/events/channel_update.py
@@ -4,7 +4,7 @@ from discord import Embed, Member, User
 from discord.abc import GuildChannel
 from winter_dragon.bot.constants import CHANGED_COLOR
 from winter_dragon.bot.events.base.audit_event import AuditEvent
-from winter_dragon.bot.events.base.util import get_differences
+from winter_dragon.bot.events.base.util import add_differences_to_embed
 
 
 class ChannelUpdate(AuditEvent):
@@ -16,8 +16,6 @@ class ChannelUpdate(AuditEvent):
     @override
     def create_embed(self) -> Embed:  # sourcery skip: extract-duplicate-method
         channel = self.entry.target
-        before = self.entry.before
-        after = self.entry.after
         user = self.entry.user
 
         if not isinstance(channel, GuildChannel):
@@ -26,14 +24,6 @@ class ChannelUpdate(AuditEvent):
             raise TypeError(msg)
         if not isinstance(user, (User, Member)):
             msg = f"User is not a discord user: {user=}"
-            self.logger.warning(msg)
-            raise TypeError(msg)
-        if not isinstance(before, GuildChannel):
-            msg = f"Before is not a guild channel: {before=}"
-            self.logger.warning(msg)
-            raise TypeError(msg)
-        if not isinstance(after, GuildChannel):
-            msg = f"After is not a guild channel: {after=}"
             self.logger.warning(msg)
             raise TypeError(msg)
 
@@ -52,18 +42,12 @@ class ChannelUpdate(AuditEvent):
             "user_limit",
         }
 
-        channel_name = None
-        if (
-            (differences := get_differences(self.entry, properties))
-            and ("name" in differences or before.name != after.name)
-        ):
-            channel_name = f"`{before.name}` to `{after.name}` for {channel.mention}"
-
-        return Embed(
+        embed= Embed(
             title="Channel Changed",
             description=(
-                f"{user.mention} changed {differences} of channel "
-                f"{channel_name or after.mention} with reason: {self.entry.reason}"
+                f"{user.mention} changed {channel.mention} with reason: {self.entry.reason}"
             ),
             color=CHANGED_COLOR,
         )
+        add_differences_to_embed(embed, self.entry, properties)
+        return embed
