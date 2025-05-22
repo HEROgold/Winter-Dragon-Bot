47eacf260e70ce3e475eafb13eea6c6c942cce3e
HEROgold <martijnwieringa28@gmail.com>
2025-05-22 19:24:57 +0200

    create association for guild and audit logs

---
+++ database/src/winter_dragon/database/tables/associations/guild_audit_log.py
@@ -0,0 +1,12 @@
+
+from sqlalchemy import Column, ForeignKey
+from sqlmodel import Field, SQLModel
+from winter_dragon.database.keys import get_foreign_key
+from winter_dragon.database.tables.audit_log import AuditLog
+from winter_dragon.database.tables.guild import Guilds
+
+
+class GuildAuditLog(SQLModel, table=True):
+
+    guild_id: int = Field(sa_column=Column(ForeignKey(get_foreign_key(Guilds, "id")), primary_key=True))
+    audit_log_id: int = Field(sa_column=Column(ForeignKey(get_foreign_key(AuditLog, "id")), primary_key=True))
