ec37b34e0686590a5cf5572f4f2cb792568368b8
HEROgold <martijnwieringa28@gmail.com>
2025-06-07 19:40:29 +0200

    Fix ID issue

--- database/src/winter_dragon/database/tables/autochannel.py
+++ database/src/winter_dragon/database/tables/autochannel.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey
+from sqlalchemy import BigInteger, Column, ForeignKey
 from sqlmodel import Field, Relationship, SQLModel
 from winter_dragon.database.keys import get_foreign_key
 from winter_dragon.database.tables.channel import Channels
@@ -6,7 +6,7 @@ from winter_dragon.database.tables.channel import Channels
 
 class AutoChannels(SQLModel, table=True):
 
-    id: int | None = Field(default=None, primary_key=True)
+    id: int = Field(sa_column=Column(BigInteger(), primary_key=True))
     channel_id: int = Field(sa_column=Column(ForeignKey(get_foreign_key(Channels, "id"))))
 
     channel: Channels = Relationship()
