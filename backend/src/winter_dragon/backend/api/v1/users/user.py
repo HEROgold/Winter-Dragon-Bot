d397139954606f796af16fdf177fa4fea56092aa
HEROgold <martijnwieringa28@gmail.com>
2025-06-07 19:18:24 +0200

    fix imports

--- backend/src/winter_dragon/backend/api/v1/users/user.py
+++ backend/src/winter_dragon/backend/api/v1/users/user.py
@@ -1,18 +1,18 @@
+"""Api endpoints for user data."""
 from collections.abc import Sequence
 
 from fastapi import APIRouter, status
 from sqlmodel import select
+from winter_dragon.bot.extensions.user.reminder import Reminder
 from winter_dragon.database import session
+from winter_dragon.database.tables.associations.user_roles import UserRoles
+from winter_dragon.database.tables.car_fuel import CarFuels
+from winter_dragon.database.tables.infractions import Infractions
+from winter_dragon.database.tables.message import Messages
+from winter_dragon.database.tables.presence import Presence
+from winter_dragon.database.tables.role import Roles
+from winter_dragon.database.tables.steamuser import SteamUsers
 from winter_dragon.database.tables.user import Users
-
-from backend.src.winter_dragon.backend.api.v1.users.user_roles import UserRoles
-from bot.src.winter_dragon.bot.extensions.user.reminder import Reminder
-from database.src.winter_dragon.database.tables.car_fuel import CarFuels
-from database.src.winter_dragon.database.tables.infractions import Infractions
-from database.src.winter_dragon.database.tables.message import Messages
-from database.src.winter_dragon.database.tables.presence import Presence
-from database.src.winter_dragon.database.tables.role import Roles
-from database.src.winter_dragon.database.tables.steamuser import SteamUsers
 
 
 router = APIRouter(prefix="/users/{user_id}", tags=["users"])
