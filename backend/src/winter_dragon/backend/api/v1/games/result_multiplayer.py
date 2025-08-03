4208be3aa994d21d86df95cf06939414847b11a1
HEROgold <martijnwieringa28@gmail.com>
2025-08-03 11:43:22 +0200

    spacing

--- backend/src/winter_dragon/backend/api/v1/games/result_multiplayer.py
+++ backend/src/winter_dragon/backend/api/v1/games/result_multiplayer.py
@@ -2,5 +2,6 @@
 from winter_dragon.database.extension.api_model import APIModel
 from winter_dragon.database.tables.result_multiplayer import ResultMassiveMultiplayer
 
+
 model = APIModel(ResultMassiveMultiplayer)
 router = model.router
