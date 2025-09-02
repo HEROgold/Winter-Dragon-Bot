115dc03c4e74f36e65d64d40e2fefe5ebdadf6a4
HEROgold <martijnwieringa28@gmail.com>
2025-09-02 20:13:03 +0200

    import fix

--- database/src/winter_dragon/database/tables/steamsale.py
+++ database/src/winter_dragon/database/tables/steamsale.py
@@ -1,9 +1,9 @@
 from datetime import UTC, datetime, timedelta
 from enum import Enum, auto
 
-from constants import session
 from sqlalchemy import text
 from sqlmodel import Field
+from winter_dragon.database.constants import session
 from winter_dragon.database.extension.model import SQLModel
 from winter_dragon.database.keys import get_foreign_key
 
