03fdadc6e075024f5b63e2ed4f9d8a0629a8edf1
HEROgold <martijnwieringa28@gmail.com>
2025-09-17 12:57:28 +0200

    add extra _ to class var

--- bot/src/winter_dragon/bot/core/log.py
+++ bot/src/winter_dragon/bot/core/log.py
@@ -12,13 +12,13 @@ class LoggerMixin:
     """Mixin class for adding loggers to classes based on their names."""
 
     logger: logging.Logger
-    _furthest_child_name = None  # Track the name of the furthest child class
+    __furthest_child_name = None  # Track the name of the furthest child class
 
     def __init_subclass__(cls, **kwargs: Any) -> None:  # noqa: ANN401
         """Initialize subclasses. Adding loggers to the class with names based on the class name."""
         super().__init_subclass__(**kwargs)
         # Update the furthest child name with the current subclass name
-        LoggerMixin._furthest_child_name = cls.__name__
+        LoggerMixin.__furthest_child_name = cls.__name__
         # Update the logger for all classes in the hierarchy to reflect the furthest child
         cls.update_logger_for_hierarchy()
 
@@ -28,7 +28,7 @@ class LoggerMixin:
         # Traverse the MRO (Method Resolution Order) in reverse to update logger from base to the furthest child
         for base in reversed(cls.mro()):
             if issubclass(base, LoggerMixin) and hasattr(base, "_furthest_child_name"):
-                base.logger = logging.getLogger(f"{Settings.bot_name}.{LoggerMixin._furthest_child_name}")
+                base.logger = logging.getLogger(f"{Settings.bot_name}.{LoggerMixin.__furthest_child_name}")
                 base.setup_logger(base.logger)
 
     @classmethod
