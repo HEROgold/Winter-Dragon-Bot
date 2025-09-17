c72954ab5b1fd1b86053f387857cf8a529e3c34d
HEROgold <martijnwieringa28@gmail.com>
2025-09-17 12:37:21 +0200

    remove unusable router

--- backend/src/winter_dragon/backend/api/v1/games/lobbystatus.py
+++
@@ -1,7 +0,0 @@
-
-from winter_dragon.database.extension.api_model import APIModel
-from winter_dragon.database.tables.lobbystatus import LobbyStatus
-
-
-model = APIModel(LobbyStatus)
-router = model.router
