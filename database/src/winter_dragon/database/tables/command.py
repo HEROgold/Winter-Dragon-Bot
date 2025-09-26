be0e0be08830ef8b3dace4b76756d3b936e1ee40
HEROgold <martijnwieringa28@gmail.com>
2025-09-27 01:20:08 +0200

    add field for autcomplete

--- database/src/winter_dragon/database/tables/command.py
+++ database/src/winter_dragon/database/tables/command.py
@@ -1,8 +1,9 @@
 
+from sqlmodel import Field
 from winter_dragon.database.extension.model import SQLModel
 
 
 class Commands(SQLModel, table=True):
 
-    qual_name: str
-    call_count: int
+    qual_name: str = Field()
+    call_count: int = Field()
