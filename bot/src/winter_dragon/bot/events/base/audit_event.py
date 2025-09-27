e227f0d75bdbdad9d7f425b2820c95ec789a75d2
HEROgold <martijnwieringa28@gmail.com>
2025-09-27 02:03:20 +0200

    Appendage to last commit. + lazily evaluate properties

--- bot/src/winter_dragon/bot/events/base/audit_event.py
+++ bot/src/winter_dragon/bot/events/base/audit_event.py
@@ -1,12 +1,23 @@
 """Module for abstracting audit events."""
-from abc import ABC, abstractmethod
+from __future__ import annotations
 
-from discord import AuditLogEntry, Embed
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
 from winter_dragon.bot.core.log import LoggerMixin
 from winter_dragon.bot.enums.channels import LogCategories
+from winter_dragon.bot.events.base.factory import AuditEventFactory
 from winter_dragon.database.tables.audit_log import AuditLog
 
 
+if TYPE_CHECKING:
+    from discord import AuditLogAction, AuditLogEntry, Embed
+
+
+# This system is limited, to only one handler per event type.
+# It's possible we want multiple handler for a single event type, with different "filters"
+# we should design for that eventuality.
+# e.g. log channel create events log to different channels based on channel type.
 class AuditEvent(ABC, LoggerMixin):
     """Base class for audit events."""
 
@@ -14,8 +25,20 @@ class AuditEvent(ABC, LoggerMixin):
         """Initialize the audit event."""
         super().__init__()
         self.entry = entry
-        self.category = LogCategories.from_AuditLogAction(entry.action)
-        self.db_entry = AuditLog.from_audit_log(entry) # creates the entry in the database, and returns the entry.
+
+    @property
+    def category(self) -> LogCategories:
+        """Get the log category. lazily evaluated."""
+        if not self._category:
+            self._category = LogCategories.from_AuditLogAction(self.entry.action)
+        return self._category
+
+    @property
+    def db_entry(self) -> AuditLog:
+        """Get the database entry. lazily evaluated."""
+        if not self._db_entry:
+            self._db_entry = AuditLog.from_audit_log(self.entry) # creates the entry in the database, and returns the entry.
+        return self._db_entry
 
     @abstractmethod
     async def handle(self) -> None:
@@ -24,3 +47,7 @@ class AuditEvent(ABC, LoggerMixin):
     @abstractmethod
     def create_embed(self) -> Embed:
         """Create an embed for the audit event."""
+
+    def __init_subclass__(cls: type[AuditEvent], action: AuditLogAction) -> None:
+        """Register the subclass with the factory."""
+        AuditEventFactory.register(action, cls)
