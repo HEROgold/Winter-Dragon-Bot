46b0281f71acb8f78335be8cbfb026257145800f
HEROgold <martijnwieringa28@gmail.com>
2026-01-11 04:07:27 +0100

    Use discord's build-in abstract types

--- src/winter_dragon/bot/errors/factory.py
+++ src/winter_dragon/bot/errors/factory.py
@@ -8,24 +8,23 @@ from typing import TYPE_CHECKING, ClassVar, cast
 if TYPE_CHECKING:
     from collections.abc import Generator
 
-    from discord import Interaction
+    from discord import DiscordException, Interaction
     from discord.app_commands.errors import AppCommandError
     from discord.ext.commands import CommandError, Context
     from discord.ext.commands.bot import BotBase
 
     from winter_dragon.bot.core.bot import BotT
 
-    from .alias import DiscordCommandError
     from .error import DiscordError
 
 
 class ErrorFactory:
     """Factory for creating Error errors."""
 
-    registry: ClassVar[dict[DiscordCommandError, list[type[DiscordError]]]] = {}
+    registry: ClassVar[dict[type[DiscordException], list[type[DiscordError]]]] = {}
 
     @classmethod
-    def register(cls, error: DiscordCommandError, error_type: type[DiscordError]) -> None:
+    def register(cls, error: type[DiscordException], error_type: type[DiscordError]) -> None:
         """Register an Error error class for a category."""
         if error not in cls.registry:
             cls.registry[error] = [error_type]
@@ -36,14 +35,15 @@ class ErrorFactory:
     def get_handlers(
         cls,
         bot: BotBase,
-        error_type: DiscordCommandError,
+        exception: DiscordException,
         *,
         interaction: Interaction | None = None,
         ctx: Context[BotT] | None = None,
     ) -> Generator[DiscordError]:
         """Get the Error error class for a category."""
-        if error_type not in cls.registry:
-            msg = f"Error for `{error_type}` not implemented"
+        exc_type = type(exception)
+        if exc_type not in cls.registry:
+            msg = f"Error for `{exc_type}` not implemented"
             raise NotImplementedError(msg)
         if not (interaction or ctx):
             msg = "Missing interaction or ctx kwarg"
@@ -52,10 +52,10 @@ class ErrorFactory:
             msg = "Cannot pass both interaction and ctx"
             raise ValueError(msg)
 
-        for handler in cls.registry[error_type]:
+        for handler in cls.registry[exc_type]:
             if interaction:
-                error_type = cast("AppCommandError", error_type)
-                yield handler(bot, interaction, error_type)
+                exc_type = cast("AppCommandError", exc_type)
+                yield handler(bot, interaction, exc_type)
             elif ctx:
-                error_type = cast("CommandError", error_type)
-                yield handler(bot, ctx, error_type)
+                exc_type = cast("CommandError", exc_type)
+                yield handler(bot, ctx, exc_type)
