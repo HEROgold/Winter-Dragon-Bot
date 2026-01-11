c23856f8ecba3856a895da42e4e58f1f62ed7432
HEROgold <martijnwieringa28@gmail.com>
2026-01-11 04:07:04 +0100

    remove unused file.

--- src/winter_dragon/bot/errors/alias.py
+++
@@ -1,7 +0,0 @@
-"""Type aliases shared by the bot's error handling helpers."""
-
-from discord.app_commands.errors import AppCommandError
-from discord.ext.commands.errors import CommandError
-
-
-type DiscordCommandError = CommandError | AppCommandError
