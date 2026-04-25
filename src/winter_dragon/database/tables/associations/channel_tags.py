a7a6835e84699e00c30c5dcfc1b7023cce3da4e4
HEROgold <martijnwieringa28@gmail.com>
2026-04-25 15:06:17 +0200

    refactor: define tag field with Enum type

--- src/winter_dragon/database/tables/associations/channel_tags.py
+++ src/winter_dragon/database/tables/associations/channel_tags.py
@@ -1,21 +1,16 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
-from sqlalchemy import Column, ForeignKey
+from sqlalchemy import Column, Enum, ForeignKey
 from sqlmodel import Field
 
+from winter_dragon.database.channel_types import Tags
 from winter_dragon.database.extension.model import SQLModel
 from winter_dragon.database.keys import get_foreign_key
 from winter_dragon.database.tables.channel import Channels
-
-
-if TYPE_CHECKING:
-    from winter_dragon.database.channel_types import Tags
 
 
 class ChannelTag(SQLModel, table=True):
     """Association table linking channels to their tags."""
 
     channel_id: int = Field(sa_column=Column(ForeignKey(get_foreign_key(Channels), ondelete="CASCADE"), primary_key=True))
-    tag: Tags
+    tag: Tags = Field(sa_column=Column(Enum(Tags), nullable=False))
